3aa7f90bce7c4f50fba3790558cdad929fb0d9fc
Lyuboslav Shishkov <lubidead1@mail.bg>
2016-06-27 13:10:19 +0300

    Option to add "cloack" behind ui element
    Cloack is basically white transparent panel. Used to prevent clicking
    firing click events on background ui elements

---
+++ Assets/Scripts/Utils/AddUICloack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AddUICloack : MonoBehaviour
+{
+    Image cloackImage = null;
+
+    void Start()
+    {
+        var canvas = GameObject.Find("Canvas");
+        var prefab = Resources.Load<Transform>("Prefabs/Cloack");
+        var cloack = Instantiate(prefab);
+
+        cloackImage = cloack.GetComponent<Image>();
+
+        cloack.name = "Cloack " + transform.name;
+        cloack.SetParent(transform.parent, false);
+        transform.SetParent(cloack, false);
+    }
+
+    void OnDisable()
+    {
+        if (cloackImage != null)
+        {
+            cloackImage.enabled = false;
+        }
+    }
+
+    void OnEnable()
+    {
+        if (cloackImage != null)
+        {
+            cloackImage.enabled = true;    
+        }
+    }
+}
