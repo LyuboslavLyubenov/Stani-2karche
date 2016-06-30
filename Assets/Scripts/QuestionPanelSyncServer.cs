32f1b4c7870df266b416d43b2ef631878a5a37c5
Lyuboslav Shishkov <lubidead1@mail.bg>
2016-06-30 16:39:23 +0300

    Now able to "simulate" player actions
    * ONLY FOR NORMAL EXAM GAME TYPE
    * NOT TESTED !!!

---
+++ Assets/Scripts/QuestionPanelSyncServer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class QuestionPanelSyncServer : MonoBehaviour
+{
+    public PlayingUIController PlayingUIController;
+   
+    ServerNetworkManager serverNetworkManager;
+
+    void Start()
+    {
+        serverNetworkManager = PlayingUIController.ServerNetworkManager;
+
+        var questionUIController = PlayingUIController.QuestionUIController;
+        questionUIController.OnAnswerClick += OnAnswerClick;
+        questionUIController.OnQuestionLoaded += OnQuestionLoaded;
+    }
+
+    void OnQuestionLoaded(object sender, QuestionEventArgs args)
+    {
+        var questionJSON = JsonUtility.ToJson(args.Question);
+        serverNetworkManager.SendAllClientsMessage("LoadQuestion:" + questionJSON);
+    }
+
+    void OnAnswerClick(object sender, AnswerEventArgs args)
+    {
+        serverNetworkManager.SendAllClientsMessage("SelectAnswer:" + args.Answer);
+    }
+}
