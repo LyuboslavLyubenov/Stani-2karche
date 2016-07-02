fb7306bfdc778a5b01d6a8fa4e2e955acc80e19c
Lyuboslav Shishkov <lubidead1@mail.bg>
2016-07-02 18:58:56 +0300

    Added notification service

---
+++ Assets/Scripts/Notifications/NotificationElementController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NotificationElementController : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Dismiss);
+    }
+
+    void OnDisable()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void Dismiss()
+    {
+        GetComponent<Animator>().SetTrigger("Dismissed");
+    }
+}
---
+++ Assets/Scripts/Notifications/NotificationsController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+
+public class NotificationsController : MonoBehaviour
+{
+    const int SpaceBetweenNotifications = 10;
+    const int StartOffset = 10;
+
+    public Transform Container;
+
+    HashSet<GameObject> allNotifications = new HashSet<GameObject>();
+
+    Transform notificationElementPrefab;
+    float notificationElementSizeY;
+    RectTransform rect;
+
+    void Start()
+    {
+        notificationElementPrefab = Resources.Load<Transform>("Prefabs/NotificationElement");
+        rect = GetComponent<RectTransform>();
+
+        notificationElementSizeY = notificationElementPrefab.GetComponent<RectTransform>().sizeDelta.y;
+    }
+
+    public void AddNotification(Color color, string message)
+    {
+        var notification = Instantiate(notificationElementPrefab);
+        var notificationRect = notification.GetComponent<RectTransform>();
+        var notificationImage = notification.GetComponent<Image>();
+        var notificationText = notification.GetComponentInChildren<Text>();
+
+        notificationImage.color = color;
+        notificationText.text = message;
+
+        notification.SetParent(Container, false);
+
+        var newY = StartOffset + (GetNotificationCount() * notificationRect.sizeDelta.y + SpaceBetweenNotifications);
+        notificationRect.anchoredPosition = new Vector2(0, -newY);
+
+        allNotifications.Add(notification.gameObject);
+    }
+
+    int GetNotificationCount()
+    {
+        return Container.childCount; 
+    }
+}
