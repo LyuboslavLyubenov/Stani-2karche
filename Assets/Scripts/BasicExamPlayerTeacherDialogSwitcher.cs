b0e713bc7577d0b295dbb3c95d6f805ea2448375
Lyuboslav Shishkov <lubidead1@mail.bg>
2016-07-14 18:52:09 +0300

    Tutorial for Basic exam game for Primary Player

---
+++ Assets/Scripts/BasicExamPlayerTeacherDialogSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System;
+
+public class BasicExamPlayerTeacherDialogSwitcher : DialogSwitcher
+{
+    protected override void Start()
+    {
+        string TeacherDialogsFilePath = Directory.GetCurrentDirectory() + "\\LevelData\\basicExamPlayerTeacherDialogs.txt";
+        base.DialogFilePath = TeacherDialogsFilePath;
+        base.Start();
+    }
+
+    public void ExplainThemeSelect()
+    {
+        CoroutineUtils.WaitForFrames(0, () =>
+            {
+                var message = base.TeacherDialogs["ExplainThemeSelect"];
+                base.DisplayMessage(message, 3);
+            });
+    }
+}
