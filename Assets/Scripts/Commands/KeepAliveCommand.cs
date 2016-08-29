f120d8a5b2ae07ff95a9374539768852005d4bbe
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-08-29 14:13:04 +0300

    KeepAliveCommand and AnswerClickedCommand

---
+++ Assets/Scripts/Commands/KeepAliveCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class KeepAliveCommand : INetworkManagerCommand
+{
+    ICollection<int> aliveClientsIds;
+
+    public KeepAliveCommand(ICollection<int> aliveClientsIds)
+    {
+        if (aliveClientsIds == null)
+        {
+            throw new System.ArgumentNullException("aliveClientsIds");
+        }
+
+        this.aliveClientsIds = aliveClientsIds;
+    }
+
+    public void Execute(System.Collections.Generic.Dictionary<string, string> commandsOptionsValues)
+    {
+        var connectionId = int.Parse(commandsOptionsValues["ConnectionId"]);
+        aliveClientsIds.Add(connectionId);
+    }
+}
