904c0fb40809adf27ff3d02786468095e5354bac
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-08-29 14:10:33 +0300

    .

--- Assets/Scripts/Commands/CommandsManager.cs
+++ Assets/Scripts/Commands/CommandsManager.cs
@@ -23,53 +23,16 @@ public class CommandsManager
         commandsToExecute.Add(commandName, commandToExecute);
     }
 
-    public void Execute(string command)
+    public void Execute(NetworkCommandData command)
     {
-        if (string.IsNullOrEmpty(command))
-        {
-            throw new ArgumentNullException("command");
-        }
-
-        var commandDelitemerIndex = command.IndexOf('-');
-
-        if (commandDelitemerIndex < 0)
-        {
-            throw new ArgumentException("Invalid command");
-        }
-
-        var commandName = command.Substring(0, commandDelitemerIndex);
+        var commandName = command.Name;
 
         if (!commandsToExecute.ContainsKey(commandName))
         {
-            throw new Exception("Command " + commandName + " not found.");
+            throw new ArgumentException("Command not found");
         }
 
-        var commandOptionsValues = new Dictionary<string, string>();
-
-        if (commandDelitemerIndex < command.Length)
-        {
-            var commandOptions = command.Substring(commandDelitemerIndex + 1)
-                .Skip(1)
-                .ToString().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
-                .ToArray();
-
-            foreach (var option in commandOptions)
-            {
-                var optionValueDelitemer = option.IndexOf('=');
-
-                if (optionValueDelitemer <= 0 || option.Length <= (optionValueDelitemer + 1))
-                {
-                    return;
-                }
-
-                var optionName = option.Substring(0, optionValueDelitemer);
-                var optionValue = option.Substring(optionValueDelitemer + 1);
-
-                commandOptionsValues.Add(optionName, optionValue);
-            }
-        }
- 
         var commandToExecute = commandsToExecute[commandName];
-        commandToExecute.Execute(commandOptionsValues);
+        commandToExecute.Execute(command.Options);
     }
 }
