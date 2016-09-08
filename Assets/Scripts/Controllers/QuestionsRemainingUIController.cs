f292f78efa88a90bab06b72f38d8381d3a83be98
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-09-08 20:10:46 +0300

    Added QuestionsRemainingUIController

---
+++ Assets/Scripts/Controllers/QuestionsRemainingUIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class QuestionsRemainingUIController : MonoBehaviour
+{
+    public Text QuestionsRemaining;
+
+    public void SetRemainingQuestions(int remainingQuestions)
+    {
+        if (remainingQuestions < 0)
+        {
+            throw new ArgumentOutOfRangeException("remainingQuestions");
+        }
+
+        QuestionsRemaining.text = remainingQuestions.ToString();
+    }
+}
