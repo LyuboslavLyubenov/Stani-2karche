59f9c9f9092dad04aacd442984f31a32ba4c96b0
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-09-08 19:55:46 +0300

    Seperate

---
+++ Assets/Scripts/Jokers/AskAudienceJoker.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Timers;
+
+public class AskAudienceJoker : IJoker
+{
+    const int MinCorrectAnswerChance = 40;
+    const int MaxCorrectAnswerChance = 85;
+
+    const int MinClientsForOnlineVote_Release = 4;
+    const int MinClientsForOnlineVote_Development = 1;
+
+    public EventHandler<AudienceVoteEventArgs> OnAudienceVoted = delegate
+    {
+    };
+
+    IGameData gameData;
+
+    ClientNetworkManager networkManager;
+
+    Dictionary<string, int> audienceAnswerVoteCount = new Dictionary<string, int>();
+    List<int> audienceVotedId = new List<int>();
+
+    int connectedClients = 0;
+
+    GameObject waitingToAnswerUI;
+    GameObject loadingUI;
+    GameObject audienceAnswerUI;
+
+    AudienceAnswerUIController audienceAnswerUIController;
+
+    Timer audienceAnswerTimeoutTimer = new Timer();
+
+    public Sprite Image
+    {
+        get;
+        private set;
+    }
+
+    public EventHandler OnActivated
+    {
+        get;
+        set;
+    }
+
+    public bool Activated
+    {
+        get;
+        private set;
+    }
+
+    public AskAudienceJoker(IGameData gameData, ClientNetworkManager networkManager, GameObject waitingToAnswerUI, GameObject audienceAnswerUI)
+    {
+        if (gameData == null)
+        {
+            throw new ArgumentNullException("gameData");
+        }
+
+        if (networkManager == null)
+        {
+            throw new ArgumentNullException("networkManager");
+        }
+
+        if (waitingToAnswerUI == null)
+        {
+            throw new ArgumentNullException("waitingToAnswerUI");
+        }
+
+        if (audienceAnswerUI == null)
+        {
+            throw new ArgumentNullException("audienceAnswerUI");
+        }
+            
+        this.gameData = gameData;
+        this.networkManager = networkManager;
+        this.waitingToAnswerUI = waitingToAnswerUI;
+        this.audienceAnswerUI = audienceAnswerUI;
+        this.audienceAnswerUIController = audienceAnswerUI.GetComponent<AudienceAnswerUIController>();
+        this.loadingUI = GameObject.FindWithTag("LoadingUI");
+
+        audienceAnswerTimeoutTimer.AutoReset = false;
+        audienceAnswerTimeoutTimer.Interval = 10000;
+        audienceAnswerTimeoutTimer.Elapsed += (sender, e) => StopReceivingAnswer(NetworkCommandData.CODE_OptionClientConnectionIdValueAll);
+        audienceAnswerTimeoutTimer.Start();
+
+        Image = Resources.Load<Sprite>("Images/Buttons/Jokers/AskAudience");
+    }
+
+    public void Activate()
+    {
+        loadingUI.SetActive(true);
+        gameData.GetCurrentQuestion(ActivateAskAudienceJoker, (exception) =>
+            {
+                loadingUI.SetActive(false);
+                Debug.LogException(exception);
+            });
+    }
+
+    void ActivateAskAudienceJoker(Question currentQuestion)
+    {
+        Activated = true;
+
+        if (OnActivated != null)
+        {
+            OnActivated(this, EventArgs.Empty);
+        }
+
+        connectedClients = networkManager.ServerConnectedClientsCount;
+
+        loadingUI.SetActive(false);
+        waitingToAnswerUI.SetActive(true);
+
+        var waitingToAnswerUIController = waitingToAnswerUI.GetComponent<WaitingToAnswerUIController>();
+        waitingToAnswerUIController.ActivateSendRemainingTimeToClientCommand(NetworkCommandData.CODE_OptionClientConnectionIdValueAll);
+        waitingToAnswerUIController.ActivateSendStopReceivingAnswerCommand(NetworkCommandData.CODE_OptionClientConnectionIdValueAll);
+
+        StartReceiveAudienceVote();
+    }
+
+    void StartReceiveAudienceVote()
+    {
+        networkManager.CommandsManager.AddCommand("AskAudienceVote", new AskAudienceVoteCommand(ReceivedAudienceVote));
+    }
+
+    public void AddVote(int connectionId, string answer)
+    {
+        ReceivedAudienceVote(connectionId, answer);
+    }
+
+    void ReceivedAudienceVote(int connectionId, string answer)
+    {
+        if (!Activated)
+        {
+            return;   
+        }
+
+        if (!audienceAnswerVoteCount.ContainsKey(answer) || audienceVotedId.Contains(connectionId))
+        {
+            StartReceiveAudienceVote();
+            return;
+        }
+
+        audienceVotedId.Add(connectionId);
+        audienceAnswerVoteCount[answer]++;
+
+        audienceAnswerTimeoutTimer.Stop();
+        audienceAnswerTimeoutTimer.Start();
+
+        //TODO: CHECK AFTER TIMEOUT
+        if (audienceVotedId.Count >= connectedClients)
+        {
+            audienceAnswerUI.SetActive(true);
+            audienceAnswerUIController.SetVoteCount(audienceAnswerVoteCount, false);
+
+            if (OnAudienceVoted != null)
+            {
+                OnAudienceVoted(this, new AudienceVoteEventArgs(audienceAnswerVoteCount));
+            }
+
+            return;
+        }
+
+        StartReceiveAudienceVote();
+    }
+}
---
+++ Assets/Scripts/Jokers/HelpFromFriendJoker.cs
@@ -0,0 +1,177 @@
+﻿using System.Linq;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class HelpFromFriendJoker : IJoker
+{
+    //0% = 0f, 100% = 1f
+    const float ChanceForGeneratingCorrectAnswer = 0.85f;
+
+    public EventHandler<AnswerEventArgs> OnFriendAnswered = delegate
+    {
+    };
+
+    IGameData gameData;
+    ClientNetworkManager networkManager;
+
+    CallAFriendUIController callAFriendUIController;
+    FriendAnswerUIController friendAnswerUIController;
+
+    GameObject callAFriendUI;
+    GameObject friendAnswerUI;
+    GameObject waitingToAnswerUI;
+    GameObject loadingUI;
+
+    public Sprite Image
+    {
+        get;
+        private set;
+    }
+
+    public EventHandler OnActivated
+    {
+        get;
+        set;
+    }
+
+    public bool Activated
+    {
+        get
+        {
+            return activated;
+        }
+    }
+
+    bool activated = false;
+
+    public HelpFromFriendJoker(IGameData gameData, 
+                               ClientNetworkManager networkManager, 
+                               GameObject callAFriendUI, 
+                               GameObject friendAnswerUI, 
+                               GameObject waitingToAnswerUI)
+    {
+        if (gameData == null)
+        {
+            throw new ArgumentNullException("gameData");
+        }
+            
+        if (networkManager == null)
+        {
+            throw new ArgumentNullException("networkManager");
+        }
+
+        if (callAFriendUI == null)
+        {
+            throw new ArgumentNullException("callAFriendUI");
+        }
+
+        if (friendAnswerUI == null)
+        {
+            throw new ArgumentNullException("friendAnswerUI");
+        }
+
+        if (waitingToAnswerUI == null)
+        {
+            throw new ArgumentNullException("waitingToAnswerUI");
+        }
+
+        this.gameData = gameData;
+        this.networkManager = networkManager;
+        this.callAFriendUI = callAFriendUI;
+        this.friendAnswerUI = friendAnswerUI;
+        this.waitingToAnswerUI = waitingToAnswerUI;
+        this.loadingUI = GameObject.FindWithTag("LoadingUI");
+
+        callAFriendUIController = callAFriendUI.GetComponent<CallAFriendUIController>();
+        friendAnswerUIController = friendAnswerUI.GetComponent<FriendAnswerUIController>();
+
+        networkManager.CommandsManager.AddCommand("AskAFriendResponse", new AskAFriendResponseCommand(OnReceivedAskAFriendResponse));
+        networkManager.CommandsManager.AddCommand("ConnectedClientsIdsNames", new ClientReceiveConnectedClientsDataCommand(OnReceivedConnectedClientsIdsNames));
+
+        Image = Resources.Load<Sprite>("Images/Buttons/Jokers/HelpFromFriend");
+    }
+
+    void OnReceivedAskAFriendResponse(string username, string answer)
+    {
+        if (!activated)
+        {
+            return;
+        }
+
+        waitingToAnswerUI.SetActive(false);
+        friendAnswerUI.SetActive(true);
+        friendAnswerUIController.SetResponse(username, answer);
+    }
+
+    void OnReceivedConnectedClientsIdsNames(OnlineClientsData_Serializable connectedClientsData)
+    {
+        if (!activated)
+        {
+            return;
+        }
+
+        loadingUI.SetActive(false);
+
+        var connectedClientsIdsNames = connectedClientsData.OnlinePlayers.ToDictionary(c => c.ConnectionId, c => c.Username);
+        callAFriendUI.SetActive(true);
+        callAFriendUIController.SetContacts(connectedClientsIdsNames);
+    }
+
+    void BeginReceiveConnectedClientsIdsNames()
+    {
+        var commandData = new NetworkCommandData("ConnectedClientsIdsNames");
+        networkManager.SendServerCommand(commandData);
+        loadingUI.SetActive(true);
+    }
+
+    void AskFriendOnline(Question question, int clientConnectionId)
+    {
+        var commandData = new NetworkCommandData("SelectedAskAFriendJoker");
+        var questionJSON = JsonUtility.ToJson(question);
+        commandData.AddOption("QuestionJSON", questionJSON);
+        commandData.AddOption("ClientConnectionId", clientConnectionId.ToString());
+
+        networkManager.SendServerCommand(commandData);
+
+        waitingToAnswerUI.SetActive(true);
+
+        var disableAfterDelayComponent = waitingToAnswerUI.GetComponent<DisableAfterDelay>();
+
+        disableAfterDelayComponent.OnTimePass += (sender, args) => SendRemainingTimeToClient(clientConnectionId, args.Seconds);
+        disableAfterDelayComponent.OnTimeEnd += (sender, args) => StopReceivingAnswer(clientConnectionId);
+    }
+
+    void StopReceivingAnswer(int clientConnectionId)
+    {
+        var answerTimeoutCommandData = new NetworkCommandData("AnswerTimeout");
+        answerTimeoutCommandData.AddOption("ClientConnectionId", clientConnectionId.ToString());
+        networkManager.SendServerCommand(answerTimeoutCommandData);
+    }
+
+    void SendRemainingTimeToClient(int clientConnectionId, int remainingTimeInSeconds)
+    {
+        var commandData = new NetworkCommandData("RemainingTimeToAnswer");
+        commandData.AddOption("TimeInSeconds", remainingTimeInSeconds.ToString());
+        commandData.AddOption("ClientConnectionId", clientConnectionId.ToString());
+        networkManager.SendServerCommand(commandData);
+    }
+
+    public void Activate()
+    {
+        gameData.GetCurrentQuestion(ActivateCallAFriendJoker, Debug.LogException);
+    }
+
+    void ActivateCallAFriendJoker(Question currentQuestion)
+    {
+        callAFriendUIController.OnCalledPlayer += (sender, args) => AskFriendOnline(currentQuestion, args.PlayerConnectionId);
+        BeginReceiveConnectedClientsIdsNames();
+
+        if (OnActivated != null)
+        {
+            OnActivated(this, EventArgs.Empty);
+        }
+
+        activated = true;
+    }
+}
