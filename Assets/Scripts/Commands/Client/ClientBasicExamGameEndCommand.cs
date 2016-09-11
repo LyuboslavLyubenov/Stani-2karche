27374254fa5ee6d5e174589b11449681c043c09d
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-09-11 15:05:26 +0300

    Commands added

---
+++ Assets/Scripts/Commands/Client/ClientBasicExamGameEndCommand.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ClientBasicExamGameEndCommand : INetworkManagerCommand
+{
+    GameObject endGameUI;
+    GameObject leaderboardUI;
+
+    EndGameUIController endGameUIController;
+    LeaderboardUIController leaderboardUIController;
+
+    public ClientBasicExamGameEndCommand(GameObject endGameUI, GameObject leaderboardUI)
+    {
+        if (endGameUI == null)
+        {
+            throw new ArgumentNullException("endGameUI");
+        }
+
+        if (leaderboardUI == null)
+        {
+            throw new ArgumentNullException("leaderboardUI");
+        }
+
+        this.endGameUI = endGameUI;
+        this.endGameUIController = endGameUI.GetComponent<EndGameUIController>();
+        this.leaderboardUI = leaderboardUI;
+        this.leaderboardUIController = leaderboardUI.GetComponent<LeaderboardUIController>();
+    }
+
+    public void Execute(Dictionary<string, string> commandsOptionsValues)
+    {
+        var mark = int.Parse(commandsOptionsValues["Mark"]);
+        var leaderboardDataJSON = commandsOptionsValues["LeaderboardDataJSON"];
+        var leaderboardData = JsonUtility.FromJson<LeaderboardData_Serializable>(leaderboardDataJSON);
+        var playersScores = leaderboardData.PlayerScore.Select(d => new PlayerScore(d.PlayerName, d.Score)).ToArray();
+
+        endGameUI.SetActive(true);
+        endGameUIController.SetMark(mark);
+
+        leaderboardUI.SetActive(true);
+        leaderboardUIController.Populate(playersScores);
+    }
+}
---
+++ Assets/Scripts/Commands/MainPlayerConnectingCommad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MainPlayerConnectingCommand : INetworkManagerCommand
+{
+    public delegate void MainPlayerConnectingDelegate(int connectionId);
+
+    MainPlayerConnectingDelegate onMainPlayerConnecting;
+
+    public MainPlayerConnectingCommand(MainPlayerConnectingDelegate onMainPlayerConnecting)
+    {
+        if (onMainPlayerConnecting == null)
+        {
+            throw new ArgumentNullException("onMainPlayerConnecting");
+        }
+
+        this.onMainPlayerConnecting = onMainPlayerConnecting;
+    }
+
+    public void Execute(Dictionary<string, string> commandsOptionsValues)
+    {
+        var connectionId = int.Parse(commandsOptionsValues["ConnectionId"]);
+        onMainPlayerConnecting(connectionId);
+    }
+
+}
