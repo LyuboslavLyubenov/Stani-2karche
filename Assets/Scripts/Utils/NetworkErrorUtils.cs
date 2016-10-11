2eb02dd03b86a40c4ef665ae1f58b8d19c6a41e0
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-10-11 17:00:25 +0300

    Network errors not translated

--- Assets/Scripts/Utils/NetworkErrorUtils.cs
+++ Assets/Scripts/Utils/NetworkErrorUtils.cs
@@ -5,77 +5,20 @@ using System.Text;
 
 public class NetworkErrorUtils
 {
-    const string SuccessfullConnected = "Успешно свързан!";
-    const string SuccessfullySendOrReceiveMessage = "Успешно предадено/получено съобщение!";
-    const string ConnectionProblem = "Проблем със свързването.";
-    const string InvalidAddress = "Невалиден ип-адрес.";
-    const string TooManyPlayers = "Домакинът няма свободни места.";
-    const string AlreadyConnected = "Не може да се свържеш към повече от 1 домакин.";
-    const string ConnectionBanned = "Домакинът е забранил да се свързваш към него.";
-    const string CheckYourNetwork = "Моля проверете дали сте свързани към интернет и опитайте отново.";
-
     NetworkErrorUtils()
     {
     }
 
     public static string GetMessage(NetworkError error)
     {
-        if (error == NetworkError.Ok)
-        {
-            return SuccessfullySendOrReceiveMessage;
-        }
-
-        var errorMessage = new StringBuilder();
-        var errorName = Enum.GetName(typeof(NetworkError), error);
-
-        errorMessage.AppendLine(errorName);
-
-        switch (error)
-        {
-            case NetworkError.DNSFailure:
-                errorMessage.AppendLine(InvalidAddress);
-                break;
-
-            case NetworkError.Timeout:
-                errorMessage.AppendLine(ConnectionProblem);
-                break;
-        }
-         
-        return errorMessage.ToString();
+        var enumName = Enum.GetName(typeof(NetworkError), error);
+        return LanguagesManager.Instance.GetValue("NetworkMessages." + enumName);
     }
 
     public static string GetMessage(NetworkConnectionError error)
     {
-        var errorMessage = new StringBuilder();
-
-        if (error == NetworkConnectionError.NoError)
-        {
-            errorMessage.AppendLine(SuccessfullConnected);
-        }
-        else
-        {
-            var errorName = Enum.GetName(typeof(NetworkConnectionError), error);
-
-            errorMessage.AppendLine(errorName);
-
-            switch (error)
-            {
-                case NetworkConnectionError.TooManyConnectedPlayers:
-                    errorMessage.AppendLine(TooManyPlayers);
-                    break;
-
-                case NetworkConnectionError.AlreadyConnectedToServer:
-                case NetworkConnectionError.AlreadyConnectedToAnotherServer:
-                    errorMessage.AppendLine(AlreadyConnected);
-                    break;
-
-                case NetworkConnectionError.ConnectionBanned:
-                    errorMessage.AppendLine(ConnectionBanned);
-                    break;
-            }
-        }
-
-        return errorMessage.ToString();
+        var enumName = Enum.GetName(typeof(NetworkConnectionError), error);
+        return LanguagesManager.Instance.GetValue("NetworkMessages." + enumName);
     }
 }
 
