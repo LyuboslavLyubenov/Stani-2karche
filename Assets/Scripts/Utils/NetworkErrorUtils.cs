3dd8270abc2de0d2cc2a82fc1d2879adc86f3178
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-10-11 19:24:17 +0300

    Notfications showing correctly

--- Assets/Scripts/Utils/NetworkErrorUtils.cs
+++ Assets/Scripts/Utils/NetworkErrorUtils.cs
@@ -12,13 +12,13 @@ public class NetworkErrorUtils
     public static string GetMessage(NetworkError error)
     {
         var enumName = Enum.GetName(typeof(NetworkError), error);
-        return LanguagesManager.Instance.GetValue("NetworkMessages." + enumName);
+        return LanguagesManager.Instance.GetValue("NetworkMessages/" + enumName);
     }
 
     public static string GetMessage(NetworkConnectionError error)
     {
         var enumName = Enum.GetName(typeof(NetworkConnectionError), error);
-        return LanguagesManager.Instance.GetValue("NetworkMessages." + enumName);
+        return LanguagesManager.Instance.GetValue("NetworkMessages/" + enumName);
     }
 }
 
