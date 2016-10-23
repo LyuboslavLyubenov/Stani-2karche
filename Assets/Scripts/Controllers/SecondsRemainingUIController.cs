5880c300bd7cf89a76018c0d4828083b278439a3
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-10-23 19:34:49 +0300

    Show seconds remaining for answer main question

---
+++ Assets/Scripts/Controllers/SecondsRemainingUIController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.UI;
+using System;
+
+public class SecondsRemainingUIController : ExtendedMonoBehaviour
+{
+    public Text SecondsText;
+
+    public int RemainingSecondsToAnswer
+    {
+        get;
+        private set;
+    }
+
+    public bool Paused
+    {
+        get;
+        set;
+    }
+
+    void Start()
+    {
+        Paused = false;
+
+        CoroutineUtils.RepeatEverySeconds(1, () =>
+            {
+                if (RemainingSecondsToAnswer > 0 && !Paused)
+                {
+                    RemainingSecondsToAnswer--;
+                }
+
+                SecondsText.text = RemainingSecondsToAnswer.ToString();
+            });
+    }
+
+    public void SetSeconds(int seconds)
+    {
+        if (seconds <= 0)
+        {
+            throw new ArgumentOutOfRangeException("seconds");
+        }
+
+        RemainingSecondsToAnswer = seconds;
+        SecondsText.text = RemainingSecondsToAnswer.ToString();
+    }
+}
