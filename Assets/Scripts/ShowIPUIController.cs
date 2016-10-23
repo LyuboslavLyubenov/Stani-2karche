ac4da32ed79d43c72758f74e5128c2bdcfff5330
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-10-23 19:34:25 +0300

    Show current local and external ip

---
+++ Assets/Scripts/ShowIPUIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ShowIPUIController : MonoBehaviour
+{
+    public Text IPText;
+
+    void Start()
+    {
+        IPText.text = NetworkUtils.GetLocalIP(); 
+        StartCoroutine(NetworkUtils.GetExternalIP(OnFoundIP, OnNetworkError));
+    }
+
+    void OnFoundIP(string ip)
+    {
+        IPText.text += Environment.NewLine;
+        IPText.text += ip;
+    }
+
+    void OnNetworkError(string errorMsg)
+    {
+        IPText.text = LanguagesManager.Instance.GetValue("Errors/NoInternetConnection");
+    }
+}
