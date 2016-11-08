217b0a65e657f0930b03c079cb40ca018bfa4637
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-11-08 19:10:01 +0200

    Get device id from server

---
+++ Assets/Scripts/Network/DeviceIndetificatorSender.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceIndetificatorSender : ExtendedMonoBehaviour
+{
+    public ClientNetworkManager NetworkManager;
+
+    void Start()
+    {
+        CoroutineUtils.WaitForFrames(0, () => Initialize());
+    }
+
+    void Initialize()
+    {
+        var getDeviceIndetificator = new DummyCommand();
+        getDeviceIndetificator.OnExecuted += (sender, args) => SendDeviceIndetificator();
+
+        NetworkManager.CommandsManager.AddCommand("GetDeviceIndetificator", getDeviceIndetificator);
+    }
+
+    void SendDeviceIndetificator()
+    {
+        var deviceINdetificatorCommand = new NetworkCommandData("DeviceIndetificator");
+        deviceINdetificatorCommand.AddOption("DeviceIndetificator", SystemInfo.deviceUniqueIdentifier);
+        NetworkManager.SendServerCommand(deviceINdetificatorCommand);
+    }
+}
