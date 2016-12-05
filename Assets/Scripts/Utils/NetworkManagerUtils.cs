f501a2ef7a5a5701f802be022d564f95435a3155
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-12-05 19:14:13 +0200

    Utility for checking if server is up

---
+++ Assets/Scripts/Utils/NetworkManagerUtils.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class NetworkManagerUtils : MonoBehaviour
+{
+    static NetworkManagerUtils instance;
+
+    public static NetworkManagerUtils Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var obj = new GameObject();
+                obj.name = "NetworkManagerUtils";
+                instance = obj.AddComponent<NetworkManagerUtils>();
+            }
+
+            return instance;
+        }
+    }
+
+    NetworkManagerUtils()
+    {
+        
+    }
+
+    public void IsServerUp(string ip, int port, Action<bool> isUp)
+    {
+        StartCoroutine(IsServerUpCoroutine(ip, port, isUp));
+    }
+
+    IEnumerator IsServerUpCoroutine(string ip, int port, Action<bool> isRunning)
+    {
+        const int MaxConnectionAttempts = 5;
+
+        var connectionConfig = new ConnectionConfig();
+        connectionConfig.MaxConnectionAttempt = MaxConnectionAttempts; 
+
+        var communicationChannel = connectionConfig.AddChannel(QosType.ReliableSequenced);
+        var topology = new HostTopology(connectionConfig, 2);
+        var genericHostId = NetworkTransport.AddHost(topology, 0);
+
+        byte error;
+        var connectionId = NetworkTransport.Connect(genericHostId, ip, port, 0, out error);
+
+        var networkError = (NetworkConnectionError)error;
+        bool isUp = false;
+
+        yield return new WaitForSeconds(1f);
+
+        int recvConnectionId;
+        int recvChannelId;
+        byte[] buffer = new byte[1024];
+        int recSize;
+        byte recError;
+        var eventType = NetworkTransport.ReceiveFromHost(genericHostId, out recvConnectionId, out recvChannelId, buffer, buffer.Length, out recSize, out recError);
+
+        isUp = eventType == NetworkEventType.ConnectEvent;
+
+        yield return new WaitForEndOfFrame();
+
+        byte disconnectError;
+        NetworkTransport.Disconnect(genericHostId, connectionId, out disconnectError);
+
+        isRunning(isUp);
+    }
+
+}
