9e788128d2bacf08ac96481a4bd685f38f24547b
Lyuboslav Lyubenov <lubidead1@mail.bg>
2016-12-18 19:20:57 +0200

    Jelly animation controller now can destroy object

---
+++ Assets/DestroyOnAnimationEnd.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyOnAnimationEnd : StateMachineBehaviour
+{
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Destroy(animator.gameObject);
+    }
+}
