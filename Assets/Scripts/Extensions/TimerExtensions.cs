60a6af60f8224eb04c540a154585d78c7f97032b
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-01-05 11:58:40 +0200

    Can reset timer (System.Timer.Timer)

---
+++ Assets/Scripts/Extensions/TimerExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Extensions
+{
+
+    using System.Timers;
+
+    public static class TimerExtensions
+    {
+        public static void Reset(this Timer timer)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
