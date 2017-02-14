ae42334e729187e4f14a96bc76f86e14eacba205
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-02-14 14:31:02 +0200

    Get GameType name moved to extension method

---
+++ Assets/Scripts/Extensions/IGameServerExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Extensions
+{
+
+    using Assets.Scripts.Interfaces;
+
+    public static class IGameServerExtensions
+    {
+        public static string GetGameTypeName(this IGameServer server)
+        {
+            return server.GetType()
+                .Name.Replace("Server", "");
+        }
+    }
+}
