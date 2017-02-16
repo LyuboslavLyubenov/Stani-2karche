05799b6028c3db331464513340179e5e8313dded
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-02-16 18:29:22 +0200

    SimpleTcpServer and SimpleTcpClient implementations for tests

---
+++ Assets/Tests/UI/Lobby/DummySimpleTcpClient.cs
@@ -0,0 +1,82 @@
+namespace Assets.Tests
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces;
+
+    public class DummySimpleTcpClient : ISimpleTcpClient
+    {
+        private readonly HashSet<string> connectedServersIps = new HashSet<string>();
+
+        public void Send(string ipAddress, string message, Action onSent = null, Action<Exception> onError = null)
+        {
+            if (!this.connectedServersIps.Contains(ipAddress))
+            {
+                if (onError != null)
+                {
+                    onError(new ArgumentException("Not connected to server"));
+                }
+            }
+            else
+            {
+                if (onSent != null)
+                {
+                    onSent();
+                }
+            }
+        }
+
+        public void ConnectTo(string ipAddress, int port, Action onConnected = null, Action<Exception> onError = null)
+        {
+            if (this.connectedServersIps.Contains(ipAddress))
+            {
+                if (onError != null)
+                {
+                    onError(new ArgumentException("Already connected"));
+                }
+            }
+            else
+            {
+                this.connectedServersIps.Add(ipAddress);
+
+                if (onConnected != null)
+                {
+                    onConnected();
+                }
+            }
+        }
+
+        public void DisconnectFrom(string ipAddress, Action onSuccess = null, Action<Exception> onError = null)
+        {
+            if (!this.connectedServersIps.Contains(ipAddress))
+            {
+                if (onError != null)
+                {
+                    onError(new ArgumentException("Not connected to " + ipAddress));
+                }
+            }
+            else
+            {
+                this.connectedServersIps.Remove(ipAddress);
+
+                if (onSuccess != null)
+                {
+                    onSuccess();
+                }
+            }
+        }
+
+        public bool IsConnectedTo(string ipAddress)
+        {
+            return this.connectedServersIps.Contains(ipAddress);
+        }
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Assets/Tests/UI/Lobby/DummySimpleTcpServer.cs
@@ -0,0 +1,78 @@
+﻿namespace Assets.Tests
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.EventArgs;
+    using Assets.Scripts.Interfaces;
+
+    public class DummySimpleTcpServer : ISimpleTcpServer
+    {        
+        public event EventHandler<IpEventArgs> OnClientConnected = delegate {};
+
+        public event EventHandler<MessageEventArgs> OnReceivedMessage = delegate {};
+        
+        public int Port { get; private set; }
+
+        private HashSet<string> connectedClients = new HashSet<string>();
+        
+        public void Disconnect(string ipAddress, Action onSuccess = null, Action<Exception> onError = null)
+        {
+            if (this.connectedClients.Contains(ipAddress))
+            {
+                if (onSuccess != null)
+                {
+                    onSuccess();
+                }
+            }
+            else
+            {
+                if (onError != null)
+                {
+                    onError(new Exception());
+                }
+            }
+        }
+
+        public bool IsClientConnected(string ipAddress)
+        {
+            return this.connectedClients.Add(ipAddress);
+        }
+
+        /// <summary>
+        /// dont use me
+        /// </summary>
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ValidateIsConnected(string ipAddress)
+        {
+            if (!this.connectedClients.Contains(ipAddress))
+            {
+                throw new ArgumentException("Client not connected");
+            }
+        }
+
+        public void FakeConnectClient(string ipAddress)
+        {
+            this.connectedClients.Add(ipAddress);
+            this.OnClientConnected(this, new IpEventArgs(ipAddress));
+        }
+
+        public void FakeDisconnectClient(string ipAddress)
+        {
+            this.ValidateIsConnected(ipAddress);
+            this.connectedClients.Remove(ipAddress);
+        }
+
+        public void FakeReceiveMessageFromClient(string ipAddress, string message)
+        {
+            this.ValidateIsConnected(ipAddress);
+            this.OnReceivedMessage(this, new MessageEventArgs(ipAddress, message));
+        }
+    }
+
+}
