29c2832959665382103c9ec0c5c5b4238c3c71bc
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-02-18 18:33:57 +0200

    Activate single state (for testing)

---
+++ Assets/Tests/StateActivator.cs
@@ -0,0 +1,24 @@
+﻿namespace Assets.Tests
+{
+
+    using Assets.Scripts.Interfaces;
+    using Assets.Scripts.StateMachine;
+
+    using UnityEngine;
+
+    using Zenject;
+
+    public class StateActivator : MonoBehaviour
+    {
+        [Inject]
+        private IState state;
+
+        private SimpleFiniteStateMachine stateMachine = new SimpleFiniteStateMachine();
+
+        void Start()
+        {
+            this.stateMachine.SetCurrentState(this.state);
+        }
+    }
+
+}
