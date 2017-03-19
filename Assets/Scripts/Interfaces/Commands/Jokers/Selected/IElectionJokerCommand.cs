2446efbcc77ac73bb5965951a02048f62e87a162
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-19 20:54:22 +0200

    Interface for jokers commands that use vote to select joker

---
+++ Assets/Scripts/Interfaces/Commands/Jokers/Selected/IElectionJokerCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Interfaces.Commands.Jokers.Selected
+{
+    using System;
+    using EventArgs;
+
+    public interface IElectionJokerCommand
+    {
+        event EventHandler OnAllPlayersSelected;
+
+        event EventHandler<ClientConnectionIdEventArgs> OnPlayerSelected;
+
+        event EventHandler OnSelectTimeout;
+    }
+}
