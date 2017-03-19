60e362be9f1473a4a3264d4271cca8049ec733c8
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-19 14:07:11 +0200

    State when client is not connected to the server

---
+++ Assets/Scripts/States/EveryBodyVsTheTeacher/Shared/NotConnectedToServerState.cs
@@ -0,0 +1,133 @@
+﻿using IClientNetworkManager = Interfaces.Network.NetworkManager.IClientNetworkManager;
+using IState = Interfaces.IState;
+
+namespace Assets.Scripts.States.EveryBodyVsTheTeacher.Shared
+{
+    using System;
+
+    using Commands;
+    using Commands.Server;
+
+    using Controllers;
+
+    using Extensions;
+
+    using Localization;
+
+    using Notifications;
+
+    using StateMachine;
+
+    using UnityEngine;
+
+    using Utils;
+    using Utils.Unity;
+
+    using EventArgs = System.EventArgs;
+
+    public class NotConnectedToServerState : IState
+    {
+        private GameObject loadingUI;
+        
+        private UnableToConnectUIController unableToConnectUIController;
+
+        private IClientNetworkManager networkManager;
+        
+        private Timer_ExecuteMethodAfterTime notFoundServerIPTimer;
+
+        public NotConnectedToServerState(
+            GameObject loadingUI,
+            UnableToConnectUIController unableToConnectUIController,
+            IClientNetworkManager networkManager)
+        {
+            if (loadingUI == null)
+            {
+                throw new ArgumentNullException("loadingUI");
+            }
+
+            if (unableToConnectUIController == null)
+            {
+                throw new ArgumentNullException("unableToConnectUIController");
+            }
+
+            if (networkManager == null)
+            {
+                throw new ArgumentNullException("networkManager");
+            }
+
+            this.loadingUI = loadingUI;
+            this.unableToConnectUIController = unableToConnectUIController;
+            this.networkManager = networkManager;
+        }
+
+        private void OnConnectedToServer(object sender, EventArgs args)
+        {
+            this.loadingUI.SetActive(false);
+            this.unableToConnectUIController.gameObject.SetActive(false);
+
+            var commandData = NetworkCommandData.From<MainPlayerConnectingCommand>();
+            this.networkManager.SendServerCommand(commandData);
+
+            var connectedMsg = LanguagesManager.Instance.GetValue("EveryBodyVsTheTeacher/ConnectedToServer");
+            NotificationsController.Instance.AddNotification(Color.blue, connectedMsg);
+        }
+
+        private void OnDisconnectedFromServer(object sender, EventArgs args)
+        {
+            this.unableToConnectUIController.gameObject.SetActive(true);
+        }
+
+        private void OnFoundServerIP(string ip)
+        {
+            this.unableToConnectUIController.ServerIP = ip;
+            this.networkManager.ConnectToHost(ip);
+        }
+
+        private void OnFoundServerIPError()
+        {
+            this.notFoundServerIPTimer.Reset();
+        }
+
+        private void GetServerIpAndConnectToServer()
+        {
+            NetworkManagerUtils.Instance.GetServerIp(this.OnFoundServerIP, this.OnFoundServerIPError);
+        }
+
+        private void AttachEventHandlers()
+        {
+            this.networkManager.OnConnectedEvent += this.OnConnectedToServer;
+            this.networkManager.OnDisconnectedEvent += this.OnDisconnectedFromServer;
+        }
+
+        private void DetachEventHandlers()
+        {
+            this.networkManager.OnConnectedEvent -= this.OnConnectedToServer;
+            this.networkManager.OnDisconnectedEvent -= this.OnDisconnectedFromServer;
+        }
+
+        private void ConfigureNotFoundServerIPTimer()
+        {
+            this.notFoundServerIPTimer = TimerUtils.ExecuteAfter(1f, this.GetServerIpAndConnectToServer);
+            this.notFoundServerIPTimer.AutoDispose = false;
+            this.notFoundServerIPTimer.RunOnUnityThread = true;
+        }
+
+        public void OnStateEnter(StateMachine stateMachine)
+        {
+            this.loadingUI.SetActive(true);
+            this.unableToConnectUIController.gameObject.SetActive(false);
+
+            this.ConfigureNotFoundServerIPTimer();
+            this.AttachEventHandlers();
+        }
+
+        public void OnStateExit(StateMachine stateMachine)
+        {
+            this.DetachEventHandlers();
+
+            this.notFoundServerIPTimer.Stop();
+            this.notFoundServerIPTimer.Dispose();
+            this.notFoundServerIPTimer = null;
+        }
+    }
+}
