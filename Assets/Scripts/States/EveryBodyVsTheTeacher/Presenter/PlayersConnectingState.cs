16ff24da1bdf59ab9699ad9b27219b14dbc20ee2
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-19 14:08:02 +0200

    PlayersConnectingState for Presenter

---
+++ Assets/Scripts/States/EveryBodyVsTheTeacher/Presenter/PlayersConnectingState.cs
@@ -0,0 +1,74 @@
+﻿using IClientNetworkManager = Interfaces.Network.NetworkManager.IClientNetworkManager;
+using IState = Interfaces.IState;
+
+namespace States.EveryBodyVsTheTeacher.Presenter
+{
+    using System;
+
+    using Assets.Scripts.Interfaces.Controllers;
+
+    using Controllers.EveryBodyVsTheTeacher.PlayersConnecting;
+
+    using Network.EveryBodyVsTheTeacher.PlayersConnectingState;
+
+    using StateMachine;
+
+    public class PlayersConnectingState : IState
+    {
+        private readonly IMainPlayersContainerUIController mainPlayersContainerUiController;
+        private readonly IAudiencePlayersContainerUIController audiencePlayersContainerUiController;
+        private readonly IClientNetworkManager networkManager;
+
+        private readonly Action onEveryBodyRequestedGameStart;
+        
+        public PlayersConnectingState(
+            IMainPlayersContainerUIController mainPlayersContainerUiController,
+            IAudiencePlayersContainerUIController audiencePlayersContainerUiController,
+            IClientNetworkManager networkManager,
+            Action onEveryBodyRequestedGameStart)
+        {
+            if (mainPlayersContainerUiController == null)
+            {
+                throw new ArgumentNullException("mainPlayersContainerUiController");
+            }
+
+            if (audiencePlayersContainerUiController == null)
+            {
+                throw new ArgumentNullException("audiencePlayersContainerUiController");
+            }
+
+            if (networkManager == null)
+            {
+                throw new ArgumentNullException("networkManager");
+            }
+
+            if (onEveryBodyRequestedGameStart == null)
+            {
+                throw new ArgumentNullException("onEveryBodyRequestedGameStart");
+            }
+
+            this.mainPlayersContainerUiController = mainPlayersContainerUiController;
+            this.audiencePlayersContainerUiController = audiencePlayersContainerUiController;
+            this.networkManager = networkManager;
+            this.onEveryBodyRequestedGameStart = onEveryBodyRequestedGameStart;
+        }
+        
+        public void OnStateEnter(StateMachine stateMachine)
+        {
+            ((MainPlayersContainerUIController)this.mainPlayersContainerUiController).gameObject.SetActive(true);
+            ((AudiencePlayersContainerUIController)this.audiencePlayersContainerUiController).gameObject.SetActive(true);
+
+            PlayersConnectingStateCommandsInitializer.InitializeCommands(
+                this.networkManager,
+                this.mainPlayersContainerUiController,
+                this.audiencePlayersContainerUiController,
+                this.onEveryBodyRequestedGameStart
+                );
+        }
+
+        public void OnStateExit(StateMachine stateMachine)
+        {
+            PlayersConnectingStateCommandsInitializer.CleanCommands(this.networkManager);
+        }
+    }
+}
