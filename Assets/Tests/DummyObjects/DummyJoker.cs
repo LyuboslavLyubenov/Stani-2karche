c8790b68ea409e0028d407c7afdd99322046be56
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-20 14:50:05 +0200

    Joker used for testing

---
+++ Assets/Tests/DummyObjects/DummyJoker.cs
@@ -0,0 +1,37 @@
+﻿namespace Assets.Tests.DummyObjects
+{
+
+    using System;
+
+    using Interfaces;
+
+    using UnityEngine;
+
+    public class DummyJoker : IJoker
+    {
+        public Sprite Image { get; set; }
+
+        public event EventHandler OnActivated = delegate {};
+
+        public event EventHandler<UnhandledExceptionEventArgs> OnError = delegate {};
+
+        public event EventHandler OnFinishedExecution = delegate {};
+
+        public bool Activated { get; private set; }
+
+        public void Activate()
+        {
+            this.Activated = true;
+            this.OnActivated(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Fires OnFinishedExecutionEvent and Activated = false
+        /// </summary>
+        public void FinishExecution()
+        {
+            this.Activated = false;
+            this.OnFinishedExecution(this, EventArgs.Empty);
+        }
+    }
+}
