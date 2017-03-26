c6947320449d53334e3bba51c55d2cd7b52e524b
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-26 18:54:13 +0300

    Kalitko UI + Controller

---
+++ Assets/Prefabs/EverybodyVsTheTeacher/Jokers/KalitkoJoker/TestCreateBoxes.cs
@@ -0,0 +1,26 @@
+﻿namespace Prefabs.EverybodyVsTheTeacher.Jokers.KalitkoJoker
+{
+
+    using Controllers.Jokers;
+
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    public class TestCreateBoxes : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        private int BoxesCount = 5;
+
+        [SerializeField]
+        private KalitkoJokerContainerUIController KalitkoJokerContainerUIController;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            this.KalitkoJokerContainerUIController.CreateBoxes(this.BoxesCount,
+                    () =>
+                    {
+                        Debug.Log("Created boxes");
+                    });
+        }
+    }
+}
