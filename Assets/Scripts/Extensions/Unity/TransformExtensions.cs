62b20d5b7187178d667922409a155fb813c63fc0
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-26 18:52:32 +0300

    Extension get all children of transform

---
+++ Assets/Scripts/Extensions/Unity/TransformExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Extensions.Unity
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public static class TransformExtensions
+    {
+        public static Transform[] GetAllChildren(this Transform transform)
+        {
+            var childrenCount = transform.childCount;
+            var result = new List<Transform>();
+
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var childTransform = transform.GetChild(i);
+                result.Add(childTransform);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/Unity/Vector3Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Extensions.Unity
+{
+
+    using UnityEngine;
+
+    public static class Vector3Extensions
+    {
+        public static Vector3 Copy(this Vector3 vector)
+        {
+            return new Vector3(vector.x, vector.y, vector.z);
+        }
+    }
+}
