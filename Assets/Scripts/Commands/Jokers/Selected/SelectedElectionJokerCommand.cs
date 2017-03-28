48a50302fceb12537e0533c4359034751ef94b37
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-28 11:36:54 +0300

    SelectedKalitkoJoker refactored -> command for generic joker election and for kalitko joker election

---
+++ Assets/Scripts/Commands/Jokers/Selected/SelectedElectionJokerCommand.cs
@@ -0,0 +1,113 @@
+﻿namespace Commands.Jokers.Selected
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Timers;
+
+    using Assets.Scripts.Interfaces.Commands.Jokers.Selected;
+
+    using EventArgs;
+
+    using Extensions;
+
+    using Interfaces.Network;
+
+    using Utils;
+
+    public abstract class SelectedElectionJokerCommand : IElectionJokerCommand
+    {
+        protected const int MinTimeTimeoutInSeconds = 10;
+
+        public event EventHandler OnAllPlayersSelected = delegate
+            {
+            };
+
+        public event EventHandler<ClientConnectionIdEventArgs> OnPlayerSelected = delegate
+            {
+            };
+
+        public event EventHandler OnSelectTimeout = delegate
+            {
+            };
+
+        private readonly IEveryBodyVsTheTeacherServer server;
+        private readonly IList<int> playersSelectedJoker = new List<int>();
+        private readonly Timer selectThisJokerTimeoutTimer;
+
+        private bool startedSelecting = false;
+
+        protected SelectedElectionJokerCommand(
+            IEveryBodyVsTheTeacherServer server,
+            int selectThisJokerTimeoutInSeconds = MinTimeTimeoutInSeconds)
+        {
+            if (server == null)
+            {
+                throw new ArgumentNullException("server");
+            }
+            
+            if (selectThisJokerTimeoutInSeconds < MinTimeTimeoutInSeconds)
+            {
+                throw new ArgumentOutOfRangeException("selectThisJokerTimeoutInSeconds");
+            }
+
+            this.server = server;
+            this.selectThisJokerTimeoutTimer = TimerUtils.ExecuteAfter(selectThisJokerTimeoutInSeconds, this.SelectThisJokerTimeout);
+
+            ((IExtendedTimer)this.selectThisJokerTimeoutTimer).RunOnUnityThread = true;
+            this.selectThisJokerTimeoutTimer.Stop();
+        }
+
+        private void SelectThisJokerTimeout()
+        {
+            this.selectThisJokerTimeoutTimer.Stop();
+            this.startedSelecting = false;
+            this.playersSelectedJoker.Clear();
+            this.OnSelectTimeout(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Executed when all mainplayers are voted for this joker
+        /// </summary>
+        protected abstract void ActivateRouter();
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            if (!this.server.StartedGame)
+            {
+                return;
+            }
+
+            var connectionId = commandsOptionsValues["ConnectionId"].ConvertTo<int>();
+
+            if (!this.server.MainPlayersConnectionIds.Contains(connectionId) ||
+                this.playersSelectedJoker.Contains(connectionId))
+            {
+                return;
+            }
+
+            this.playersSelectedJoker.Add(connectionId);
+            this.OnPlayerSelected(this, new ClientConnectionIdEventArgs(connectionId));
+
+            if (!this.startedSelecting)
+            {
+                this.selectThisJokerTimeoutTimer.Start();
+                this.startedSelecting = true;
+                return;
+            }
+
+            if (this.server.MainPlayersConnectionIds.All(this.playersSelectedJoker.Contains))
+            {
+                this.selectThisJokerTimeoutTimer.Stop();
+                this.startedSelecting = false;
+
+                this.ActivateRouter();
+
+                this.OnAllPlayersSelected(this, EventArgs.Empty);
+                return;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Commands/Jokers/Selected/SelectedKalitkoJokerCommand.cs
+++ Assets/Scripts/Commands/Jokers/Selected/SelectedKalitkoJokerCommand.cs
@@ -1,115 +1,31 @@
 ﻿namespace Commands.Jokers.Selected
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Timers;
-
-    using Assets.Scripts.Interfaces.Commands.Jokers.Selected;
-
-    using EventArgs;
-
-    using Extensions;
 
     using Interfaces.Network;
     using Interfaces.Network.Jokers.Routers;
-    using Interfaces.Network.NetworkManager;
 
-    using Utils;
-
-    public class SelectedKalitkoJokerCommand : IElectionJokerCommand
+    public class SelectedKalitkoJokerCommand : SelectedElectionJokerCommand
     {
-        public const int MinTimeTimeoutInSeconds = 10;
-
-        public event EventHandler OnAllPlayersSelected = delegate
-            { };
-
-        public event EventHandler<ClientConnectionIdEventArgs> OnPlayerSelected = delegate
-            { };
-
-        public event EventHandler OnSelectTimeout = delegate
-            { };
-
-        private readonly IEveryBodyVsTheTeacherServer server;
-
         private readonly IKalitkoJokerRouter kalitkoJokerRouter;
-
-        private readonly IList<int> playersSelectedJoker = new List<int>();
-        private readonly Timer selectThisJokerTimeoutTimer;
-
-        private bool startedSelecting = false;
 
         public SelectedKalitkoJokerCommand(
             IEveryBodyVsTheTeacherServer server,
-            IKalitkoJokerRouter kalitkoJokerRouter,
-            int selectThisJokerTimeoutInSeconds = MinTimeTimeoutInSeconds)
+            IKalitkoJokerRouter kalitkoJokerRouter,             
+            int selectThisJokerTimeoutInSeconds = SelectedElectionJokerCommand.MinTimeTimeoutInSeconds)
+            : base(server, selectThisJokerTimeoutInSeconds)
         {
-            if (server == null)
-            {
-                throw new ArgumentNullException("server");
-            }
-
-            if (kalitkoJokerRouter == null)
+            if (this.kalitkoJokerRouter == null)
             {
                 throw new ArgumentNullException("kalitkoJokerRouter");
             }
 
-            if (selectThisJokerTimeoutInSeconds < MinTimeTimeoutInSeconds)
-            {
-                throw new ArgumentOutOfRangeException("selectThisJokerTimeoutInSeconds");
-            }
-
-            this.server = server;
             this.kalitkoJokerRouter = kalitkoJokerRouter;
-            this.selectThisJokerTimeoutTimer = TimerUtils.ExecuteAfter(selectThisJokerTimeoutInSeconds, this.SelectThisJokerTimeout);
-
-            ((IExtendedTimer)this.selectThisJokerTimeoutTimer).RunOnUnityThread = true;
-            this.selectThisJokerTimeoutTimer.Stop();
         }
 
-        private void SelectThisJokerTimeout()
+        protected override void ActivateRouter()
         {
-            this.selectThisJokerTimeoutTimer.Stop();
-            this.startedSelecting = false;
-            this.playersSelectedJoker.Clear();
-            this.OnSelectTimeout(this, EventArgs.Empty);
-        }
-
-        public void Execute(Dictionary<string, string> commandsOptionsValues)
-        {
-            if (!this.server.StartedGame)
-            {
-                return;
-            }
-
-            var connectionId = commandsOptionsValues["ConnectionId"].ConvertTo<int>();
-
-            if (!this.server.MainPlayersConnectionIds.Contains(connectionId) ||
-                this.playersSelectedJoker.Contains(connectionId))
-            {
-                return;
-            }
-
-            this.playersSelectedJoker.Add(connectionId);
-            this.OnPlayerSelected(this, new ClientConnectionIdEventArgs(connectionId));
-
-            if (!this.startedSelecting)
-            {
-                this.selectThisJokerTimeoutTimer.Start();
-                this.startedSelecting = true;
-                return;
-            }
-
-            if (this.server.MainPlayersConnectionIds.All(this.playersSelectedJoker.Contains))
-            {
-                this.selectThisJokerTimeoutTimer.Stop();
-                this.startedSelecting = false;
-
-                this.kalitkoJokerRouter.Activate();
-
-                this.OnAllPlayersSelected(this, EventArgs.Empty);
-                return;
-            }
+            this.kalitkoJokerRouter.Activate();
         }
     }
 }
