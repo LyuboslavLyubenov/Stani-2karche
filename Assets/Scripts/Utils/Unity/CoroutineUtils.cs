9e0375bcfbfda4034c09a973ea2a592de3872027
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-03-29 11:21:12 +0300

    Repeat method every nth frames

--- Assets/Scripts/Utils/Unity/CoroutineUtils.cs
+++ Assets/Scripts/Utils/Unity/CoroutineUtils.cs
@@ -19,30 +19,18 @@
 
             this.instance = instance;
         }
-
-        public void WaitForSeconds(float seconds, Action callback)
+        
+        private IEnumerator RepeatEveryNthFrameCoroutine(int frame, Action callback)
         {
-            this.instance.StartCoroutine(this.WaitForSecondsCoroutine(seconds, callback));
-        }
+            while (true)
+            {
+                for (int i = 0; i < frame; i++)
+                {
+                    yield return null;
+                }
 
-        public void WaitForFrames(int frames, Action callback)
-        {
-            this.instance.StartCoroutine(this.WaitForFramesCoroutine(frames, callback));
-        }
-
-        public void WaitForRenderGUIFrames(int frames, Action callback)
-        {
-            this.instance.StartCoroutine(this.WaitForGUIRenderFrameCoroutine(frames, callback));
-        }
-
-        public void WaitUntil(Func<bool> condition, Action callback)
-        {
-            this.instance.StartCoroutine(this.WaitUntilCoroutine(condition, callback));
-        }
-
-        public void RepeatEverySeconds(float seconds, Action callback)
-        {
-            this.instance.StartCoroutine(this.RepeatEverySecondsCoroutine(seconds, callback));
+                callback();
+            }
         }
 
         private IEnumerator RepeatEverySecondsCoroutine(float seconds, Action callback)
@@ -85,7 +73,45 @@
             yield return new WaitForSeconds(seconds);
             callback();
         }
+
+        public void WaitForSeconds(float seconds, Action callback)
+        {
+            this.instance.StartCoroutine(this.WaitForSecondsCoroutine(seconds, callback));
+        }
+
+        public void WaitForFrames(int frames, Action callback)
+        {
+            this.instance.StartCoroutine(this.WaitForFramesCoroutine(frames, callback));
+        }
+
+        public void WaitForRenderGUIFrames(int frames, Action callback)
+        {
+            this.instance.StartCoroutine(this.WaitForGUIRenderFrameCoroutine(frames, callback));
+        }
+
+        public void WaitUntil(Func<bool> condition, Action callback)
+        {
+            this.instance.StartCoroutine(this.WaitUntilCoroutine(condition, callback));
+        }
+
+        public void RepeatEveryNthFrame(int frame, Action callback)
+        {
+            if (frame <= 0)
+            {
+                throw new ArgumentOutOfRangeException("frame");
+            }
+
+            if (callback == null)
+            {
+                throw new ArgumentNullException("callback");
+            }
+
+            this.instance.StartCoroutine(this.RepeatEveryNthFrameCoroutine(frame, callback));
+        }
+
+        public void RepeatEverySeconds(float seconds, Action callback)
+        {
+            this.instance.StartCoroutine(this.RepeatEverySecondsCoroutine(seconds, callback));
+        }
     }
-
 }
-
