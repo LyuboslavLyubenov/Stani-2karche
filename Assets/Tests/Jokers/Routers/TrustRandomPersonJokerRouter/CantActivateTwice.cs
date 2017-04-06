6e65a28ab25d4800d4bff43c5cedc6a3a865ef4e
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-06 19:22:07 +0300

    .

---
+++ Assets/Tests/Jokers/Routers/TrustRandomPersonJokerRouter/CantActivateTwice.cs
@@ -0,0 +1,36 @@
+﻿using DummyServerNetworkManager = Tests.DummyObjects.DummyServerNetworkManager;
+
+namespace Tests.Jokers.Routers.TrustRandomPersonJokerRouter
+{
+
+    using Assets.Scripts.Interfaces.Network.Jokers.Routers;
+
+    using Extensions;
+
+    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
+
+    using Tests.DummyObjects;
+
+    using UnityEngine;
+
+    using Zenject.Source.Usage;
+
+    public class CantActivateTwice : MonoBehaviour
+    {
+        [Inject]
+        private IServerNetworkManager networkManager;
+
+        [Inject]
+        private IEveryBodyVsTheTeacherServer server;
+
+        [Inject]
+        private ITrustRandomPersonJokerRouter jokerRouter;
+
+        void Start()
+        {
+            this.jokerRouter.Activate();
+            this.jokerRouter.Activate();
+        }
+    }
+}
