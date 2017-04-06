19bd0028a24310094de28001f3c68d33667260ba
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-06 19:25:17 +0300

    Command for selecting TrustRandomPersonJoker (server side)

---
+++ Assets/Scripts/Commands/Jokers/Selected/SelectedTrustRandomPersonJokerCommand.cs
@@ -0,0 +1,32 @@
+﻿using IEveryBodyVsTheTeacherServer = Interfaces.Network.IEveryBodyVsTheTeacherServer;
+using SelectedElectionJokerCommand = Commands.Jokers.Selected.SelectedElectionJokerCommand;
+
+namespace Assets.Scripts.Commands.Jokers.Selected
+{
+    using System;
+    using Assets.Scripts.Interfaces.Network.Jokers.Routers;
+    
+    public class SelectedTrustRandomPersonJokerCommand : SelectedElectionJokerCommand
+    {
+        private readonly ITrustRandomPersonJokerRouter jokerRouter;
+
+        public SelectedTrustRandomPersonJokerCommand(
+            IEveryBodyVsTheTeacherServer server, 
+            ITrustRandomPersonJokerRouter jokerRouter,
+            int selectThisJokerTimeoutInSeconds = MinTimeTimeoutInSeconds)
+            : base(server, selectThisJokerTimeoutInSeconds)
+        {
+            if (jokerRouter == null)
+            {
+                throw new ArgumentNullException("jokerRouter");
+            }
+
+            this.jokerRouter = jokerRouter;
+        }
+
+        protected override void ActivateRouter()
+        {
+            this.jokerRouter.Activate();
+        }
+    }
+}
