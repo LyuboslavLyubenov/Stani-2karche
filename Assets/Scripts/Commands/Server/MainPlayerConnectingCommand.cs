98e4a7330a7d412cdc498d95fb1de13e30a5f12f
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-04-30 15:08:58 +0300

    Logic for connected user encapsulated into PlayerConnecingCommand. Extracted from MainPlayerConnectingCommand

--- Assets/Scripts/Commands/Server/MainPlayerConnectingCommand.cs
+++ Assets/Scripts/Commands/Server/MainPlayerConnectingCommand.cs
@@ -1,33 +1,12 @@
 ﻿namespace Commands.Server
 {
 
-    using System;
-    using System.Collections.Generic;
-
-    using Interfaces.Network.NetworkManager;
-
-    public class MainPlayerConnectingCommand : INetworkManagerCommand
+    public class MainPlayerConnectingCommand : PlayerConnectingCommand
     {
-        public delegate void MainPlayerConnectingDelegate(int connectionId);
-
-        private MainPlayerConnectingDelegate onMainPlayerConnecting;
-
-        public MainPlayerConnectingCommand(MainPlayerConnectingDelegate onMainPlayerConnecting)
+        public MainPlayerConnectingCommand(PlayerConnectingDelegate onPlayerConnecting)
+            : base(onPlayerConnecting)
         {
-            if (onMainPlayerConnecting == null)
-            {
-                throw new ArgumentNullException("onMainPlayerConnecting");
-            }
-
-            this.onMainPlayerConnecting = onMainPlayerConnecting;
         }
-
-        public void Execute(Dictionary<string, string> commandsOptionsValues)
-        {
-            var connectionId = int.Parse(commandsOptionsValues["ConnectionId"]);
-            this.onMainPlayerConnecting(connectionId);
-        }
-
     }
 
 }
---
+++ Assets/Scripts/Commands/Server/PlayerConnectingCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace Commands.Server
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Interfaces.Network.NetworkManager;
+
+    public class PlayerConnectingCommand : INetworkManagerCommand
+    {
+        public delegate void PlayerConnectingDelegate(int connectionId);
+
+        private PlayerConnectingDelegate onPlayerConnecting;
+
+        public PlayerConnectingCommand(PlayerConnectingDelegate onPlayerConnecting)
+        {
+            if (onPlayerConnecting == null)
+            {
+                throw new ArgumentNullException("onPlayerConnecting");
+            }
+
+            this.onPlayerConnecting = onPlayerConnecting;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            var connectionId = int.Parse(commandsOptionsValues["ConnectionId"]);
+            this.onPlayerConnecting(connectionId);
+        }
+    }
+
+}
