8dcf952b52afeae89827bad5657ca24bb17b09e8
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-02 12:31:58 +0300

    DummyAvailableJokersUIController

---
+++ Assets/Tests/DummyObjects/UIControllers/DummyAvailableJokersUIController.cs
@@ -0,0 +1,55 @@
+﻿namespace Assets.Tests.DummyObjects.UIControllers
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces;
+    using Assets.Scripts.Interfaces.Controllers;
+
+    using EventArgs;
+
+    public class DummyAvailableJokersUIController : IAvailableJokersUIController
+    {
+        public event EventHandler<JokerEventArgs> OnAddedJoker = delegate { };
+        public event EventHandler<JokerEventArgs> OnUsedJoker = delegate { };
+
+        private List<IJoker> jokers = new List<IJoker>();
+
+        public int JokersCount
+        {
+            get
+            {
+                return this.jokers.Count;
+            }
+        }
+
+        public void AddJoker(IJoker joker)
+        {
+            if (this.jokers.Contains(joker))
+            {
+                throw new ArgumentException();
+            }
+
+            this.jokers.Add(joker);
+            this.OnAddedJoker(this, new JokerEventArgs(joker));
+        }
+
+        public void ClearAll()
+        {
+            this.jokers.Clear();
+        }
+
+        public void FakeUsedJoker(IJoker joker)
+        {
+            if (!this.jokers.Contains(joker))
+            {
+                throw new ArgumentException();
+            }
+
+            this.jokers.Remove(joker);
+            this.OnUsedJoker(this, new JokerEventArgs(joker));
+        }
+    }
+
+}
