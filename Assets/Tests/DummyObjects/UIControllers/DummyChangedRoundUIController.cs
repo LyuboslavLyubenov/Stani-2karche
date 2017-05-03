09cac4ea2fa913a055038c6bf311838c64aaddd6
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-03 16:59:00 +0300

    DummyChangedRoundUIController

---
+++ Assets/Tests/DummyObjects/UIControllers/DummyChangedRoundUIController.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Tests.DummyObjects.UIControllers
+{
+
+    using Assets.Scripts.Interfaces.Controllers;
+
+    public class DummyChangedRoundUIController : IChangedRoundUIController
+    {
+        public int Round { get; set; }
+    }
+}
