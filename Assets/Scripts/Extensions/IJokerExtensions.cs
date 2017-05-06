222c3416338591db7176c6de5913bb4c13c75547
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-06 07:51:27 +0300

    JokerExtensions get joker name method

---
+++ Assets/Scripts/Extensions/IJokerExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Extensions
+{
+
+    using System;
+
+    using Assets.Scripts.Interfaces;
+
+    public static class IJokerExtensions
+    {
+        public static string GetName(this IJoker joker)
+        {
+            if (joker == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return joker.GetType()
+                .Name.Replace("Joker", "");
+        }
+    }
+}
