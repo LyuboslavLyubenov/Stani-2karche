1972af7f4fecbf74af8dfa02ba11f2589c8e75bc
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-06 03:26:20 +0300

    DummyKalitkoJokerUI

---
+++ Assets/Tests/DummyObjects/UIControllers/DummyKalitkoJokerUIContainer.cs
@@ -0,0 +1,25 @@
+﻿namespace Assets.Tests.DummyObjects.UIControllers
+{
+
+    using System;
+
+    using Assets.Scripts.Interfaces.Controllers;
+
+    using EventArgs;
+
+    public class DummyKalitkoJokerUIContainer : IKalitkoJokerUIController
+    {
+        public event EventHandler<AnswerEventArgs> OnShowAnswer = delegate { };
+        public event EventHandler OnShowNothing = delegate { };
+
+        public void ShowAnswer(string answer)
+        {
+            this.OnShowAnswer(this, new AnswerEventArgs(answer, null));
+        }
+
+        public void ShowNothing()
+        {
+            this.OnShowNothing(this, EventArgs.Empty);
+        }
+    }
+}
