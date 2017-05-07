e8becfa01dee6c565803642bcbf674d875c17348
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-07 20:45:18 +0300

    Command for adding kalitko joker

---
+++ Assets/Scripts/Commands/Jokers/Add/EveryBodyVsTheTeacher/Presenter/AddKalitkoJokerCommand.cs
@@ -0,0 +1,34 @@
+﻿using AddJokerAbstractCommand = Commands.Jokers.Add.AddJokerAbstractCommand;
+using IClientNetworkManager = Interfaces.Network.NetworkManager.IClientNetworkManager;
+
+namespace Assets.Scripts.Commands.Jokers.Add.EveryBodyVsTheTeacher.Presenter
+{
+
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces;
+    using Assets.Scripts.Interfaces.Controllers;
+    using Assets.Scripts.Jokers.Kalitko;
+
+    using UnityEngine;
+
+    public class AddKalitkoJokerCommand : AddJokerAbstractCommand
+    {
+        private readonly IJoker joker;
+
+        public AddKalitkoJokerCommand(
+            IAvailableJokersUIController jokersUIController, 
+            IClientNetworkManager networkManager,
+            IKalitkoJokerUIController kalitkoJokerUIController,
+            GameObject kalitkoJokerUI)
+            : base(jokersUIController)
+        {
+            this.joker = new PresenterKalitkoJoker(networkManager, kalitkoJokerUIController, kalitkoJokerUI);       
+        }
+
+        public override void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            base.AddJoker(this.joker);
+        }
+    }
+}
