2f8a01b8f17de6c9701703a9be9f79f73208d128
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-08 17:02:39 +0300

    PlayerAnswerUIController for testing

---
+++ Assets/Tests/DummyObjects/UIControllers/DummyPlayerAnswerUIController.cs
@@ -0,0 +1,32 @@
+﻿namespace Assets.Tests.DummyObjects.UIControllers
+{
+
+    using Assets.Scripts.Interfaces.Controllers;
+    using Assets.Scripts.Utils;
+
+    public class DummyPlayerAnswerUIController : IPlayerAnswerUIController
+    {
+        public string Username { get; private set; }
+
+        public string Answer { get; private set; }
+
+        public void SetResponse(string username, string answer)
+        {
+            this.Username = username;
+            this.Answer = answer;
+        }
+
+        public void SetResponse(string answer)
+        {
+            var usernames = new string[]
+                            {
+                                "Ivan",
+                                "Georgi",
+                                "Pesho"
+                            };
+
+            this.Username = usernames.GetRandomElement();
+            this.Answer = answer;
+        }
+    }
+}
