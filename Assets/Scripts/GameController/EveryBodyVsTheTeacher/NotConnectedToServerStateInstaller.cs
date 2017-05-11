50be84ff07f3ceb795047673eb7e002a409cc792
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-11 13:01:53 +0300

    Installer for NotConnectedToServerState

---
+++ Assets/Scripts/GameController/EveryBodyVsTheTeacher/NotConnectedToServerStateInstaller.cs
@@ -0,0 +1,38 @@
+﻿using IClientNetworkManager = Interfaces.Network.NetworkManager.IClientNetworkManager;
+using NotConnectedToServerState = States.EveryBodyVsTheTeacher.Shared.NotConnectedToServerState;
+using UnableToConnectUIController = Controllers.UnableToConnectUIController;
+
+namespace Assets.Scripts.GameController.EveryBodyVsTheTeacher
+{
+
+    using UnityEngine;
+
+    using Zenject.Source.Install;
+
+    public class NotConnectedToServerStateInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private GameObject loadingUI;
+        
+        [SerializeField]
+        private GameObject unableToConnectUI;
+
+        public override void InstallBindings()
+        {
+            var clientNetworkManager = this.Container.Resolve<IClientNetworkManager>();
+            var unableToConnectUIController = this.unableToConnectUI.GetComponent<UnableToConnectUIController>();
+
+            var notConnectedToServerState =
+                new NotConnectedToServerState(
+                    this.loadingUI,
+                    this.unableToConnectUI,
+                    unableToConnectUIController,
+                    clientNetworkManager);
+
+            this.Container.Bind<NotConnectedToServerState>()
+                .FromInstance(notConnectedToServerState)
+                .AsSingle();
+        }
+    }
+
+}
