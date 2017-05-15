250bea8511a59476b452fa0a29f4664a532319f9
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-15 19:53:57 +0300

    GameStartedCommand

---
+++ Assets/Scripts/Commands/EveryBodyVsTheTeacher/Shared/GameStartedCommand.cs
@@ -0,0 +1,30 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Commands.EveryBodyVsTheTeacher.Shared
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    public class GameStartedCommand : INetworkManagerCommand
+    {
+        private readonly GameObject playingUI;
+
+        public GameStartedCommand(GameObject playingUI)
+        {
+            if (playingUI == null)
+            {
+                throw new ArgumentNullException("playingUI");
+            }
+
+            this.playingUI = playingUI;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            this.playingUI.SetActive(true);
+        }
+    }
+}
