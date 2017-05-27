392e7b01fe92edf38e6077662cffb46a2071be98
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-27 19:23:01 +0300

    DummyAnswerPollResultRetriever

---
+++ Assets/Tests/DummyObjects/Network/DummyAnswerPollResultRetriever.cs
@@ -0,0 +1,48 @@
+﻿namespace Assets.Tests.DummyObjects.Network
+{
+    using System;
+    using System.Collections.Generic;
+
+    using EventArgs;
+
+    using Interfaces.Network.Jokers;
+    
+    public class DummyAnswerPollResultRetriever : IAnswerPollResultRetriever
+    {
+        public event EventHandler OnReceivedVoteTimeout = delegate {};
+        public event EventHandler OnReceivedSettingsTimeout = delegate {};
+        public event EventHandler<JokerSettingsEventArgs> OnReceivedSettings = delegate {};
+        public event EventHandler<VoteEventArgs> OnVoted = delegate {};
+
+        public bool Activated { get; private set; }
+
+        public void SimulateRecevedVoteTimeout()
+        {
+            this.OnReceivedVoteTimeout(this, EventArgs.Empty);
+        }
+
+        public void SimulateReceivedSettingsTimeout()
+        {
+            this.OnReceivedSettingsTimeout(this, EventArgs.Empty);
+        }
+
+        public void SimulateReceivedSettings(int timeToAnswerInSeconds)
+        {
+            this.OnReceivedSettings(this, new JokerSettingsEventArgs(timeToAnswerInSeconds));
+        }
+
+        public void SimulateOnVoted(Dictionary<string, int> answersVotes)
+        {
+            this.OnVoted(this, new VoteEventArgs(answersVotes));
+        }
+
+        public void Activate()
+        {
+            this.Activated = true;
+        }
+        
+        public void Dispose()
+        {
+        }
+    }
+}
