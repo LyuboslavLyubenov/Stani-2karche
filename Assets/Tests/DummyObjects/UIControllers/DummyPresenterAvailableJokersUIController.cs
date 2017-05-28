c6f25d2a3df94413d4dec05d014170000df45790
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-05-28 16:56:58 +0300

    PresenterAvailableJokersUIController

---
+++ Assets/Tests/DummyObjects/UIControllers/DummyPresenterAvailableJokersUIController.cs
@@ -0,0 +1,11 @@
+namespace Assets.Tests.DummyObjects.UIControllers
+{
+
+    public class DummyPresenterAvailableJokersUIController : DummyAvailableJokersUIController, Scripts.Interfaces.Controllers.EveryBodyVsTheTeacher.Presenter.IAvailableJokersUIController
+    {
+        public void Dispose()
+        {   
+        }
+    }
+
+}
