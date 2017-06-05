fd08d0a0caadf4f8ae8b7dd627c03c25976c90cb
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-05 17:58:15 +0300

    IsOnScreen test for AudiencePlayersContainerUI

---
+++ Assets/Tests/UI/EverybodyVsTheTeacher/AudiencePlayersContainerUI/IsOnScreen.cs
@@ -0,0 +1,63 @@
+﻿using ObjectsPool = Utils.Unity.ObjectsPool;
+
+namespace Assets.Tests.UI.EverybodyVsTheTeacher.AudiencePlayersContainerUI
+{
+    using System.Collections;
+
+    using Interfaces.Controllers;
+
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject.Source.Usage;
+
+    public class IsOnScreen : MonoBehaviour
+    {
+        [Inject]
+        private IAudiencePlayersContainerUIController audiencePlayersContainerUIController;
+
+        void Start()
+        {
+            this.StartCoroutine(this.StartTestCoroutine());
+        }
+
+        IEnumerator StartTestCoroutine()
+        {
+            this.audiencePlayersContainerUIController.ShowAudiencePlayer(1, "Player 1");
+
+            yield return new WaitForSeconds(2f);
+
+            if (!this.audiencePlayersContainerUIController.IsOnScreen(1))
+            {
+                IntegrationTest.Fail();
+                yield break;
+            }
+
+            var objectsPool = GameObject.FindObjectOfType<ObjectsPool>()
+                .gameObject
+                .transform;
+
+            for (int i = 0; i < objectsPool.childCount; i++)
+            {
+                var audiencePlayerObject = objectsPool.GetChild(i).gameObject;
+
+                if (!audiencePlayerObject.activeSelf)
+                {
+                    continue;
+                }
+
+                var audiencePlayerUsername = audiencePlayerObject.GetComponentInChildren<Text>().text;
+
+                if (audiencePlayerUsername != "Player 1")
+                {
+                    IntegrationTest.Fail();
+                    yield break;
+                }
+            }
+
+            IntegrationTest.Pass();
+        }
+    }
+}
