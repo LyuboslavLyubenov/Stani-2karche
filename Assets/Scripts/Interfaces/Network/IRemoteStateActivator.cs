5dfb442d622cd3dbd61ac6faca5a0f3fcd18cf38
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-08 11:42:19 +0300

    RemoteStateActivator interface

---
+++ Assets/Scripts/Interfaces/Network/IRemoteStateActivator.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Interfaces.Network
+{
+    using System;
+
+    public interface IRemoteStateActivator : IDisposable
+    {
+        void Bind(string id, IState stateToActivate);
+
+        void UnBind(string id);
+
+        void UnBind(IState state); 
+    }
+}
