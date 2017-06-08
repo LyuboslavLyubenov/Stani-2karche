e90553ecb83be1cff5e62ea70b60ea42447397c8
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-08 11:39:43 +0300

    ActivateStateCommand

---
+++ Assets/Scripts/Commands/ActivateStateCommand.cs
@@ -0,0 +1,39 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Commands
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces;
+
+    using StateMachine;
+
+    public class ActivateStateCommand : INetworkManagerCommand
+    {
+        private readonly StateMachine stateMachine;
+        private readonly IState state;
+
+        public ActivateStateCommand(StateMachine stateMachine, IState state)
+        {
+            if (stateMachine == null)
+            {
+                throw new ArgumentNullException("stateMachine");
+            }
+
+            if (state == null)
+            {
+                throw new ArgumentNullException("state");
+            }
+
+            this.stateMachine = stateMachine;
+            this.state = state;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            this.stateMachine.SetCurrentState(this.state);
+        }
+    }
+}
