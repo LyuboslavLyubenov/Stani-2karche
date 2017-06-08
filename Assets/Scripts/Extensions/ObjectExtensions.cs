da4303529dd0436d8ab26b6d32b4b35cc89400a7
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-08 16:44:41 +0300

    Is object containing interface extension

---
+++ Assets/Scripts/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Extensions
+{
+
+    using System;
+    using System.Linq;
+
+    public static class ObjectExtensions
+    {
+        public static bool IsImplemetingInterface(this object obj, Type interfaceType)
+        {
+            return obj.GetType()
+                .GetInterfaces()
+                .Contains(interfaceType);
+        }
+    }
+}
