e4fb43bcefad3d8e227f7a1b12ea4c6f960ccc31
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-08 23:34:42 +0300

    CurrentQuestionAnswersCollector new tests

---
+++ Assets/Tests/Network/VoteForAnswerForCurrentQuestionColletor/WhenMainPlayerReconnectedSendQuestionWithRemainingTime.cs
@@ -0,0 +1,78 @@
+﻿using DummyEveryBodyVsTheTeacherServer = Tests.DummyObjects.DummyEveryBodyVsTheTeacherServer;
+using DummyServerNetworkManager = Tests.DummyObjects.DummyServerNetworkManager;
+using ExtendedMonoBehaviour = Utils.Unity.ExtendedMonoBehaviour;
+
+namespace Assets.Tests.Network.VoteForAnswerForCurrentQuestionColletor
+{
+
+    using Commands;
+
+    using Extensions;
+
+    using Interfaces;
+    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
+
+    using UnityEngine;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject.Source.Usage;
+
+    public class WhenMainPlayerReconnectedSendQuestionWithRemainingTime : ExtendedMonoBehaviour
+    {
+        [Inject]
+        private IClientNetworkManager networkManager;
+
+        [Inject]
+        private IEveryBodyVsTheTeacherServer server;
+
+        [Inject]
+        private ICollectVoteResultForAnswerForCurrentQuestion voteResultCollector;
+
+        [Inject]
+        private ISimpleQuestion question;
+
+        [Inject]
+        private int timeToAnswer;
+
+        void Start()
+        {
+            var dummyServer = (DummyEveryBodyVsTheTeacherServer)this.server;
+            dummyServer.MainPlayersConnectionIds = new int[] { 1, 2, 3, 4, 5 };
+            dummyServer.PresenterId = 6;
+
+            this.voteResultCollector.StartCollecting();
+
+            this.CoroutineUtils.WaitForSeconds(1,
+                () =>
+                    {
+                        var questionDto = this.question.Serialize();
+                        var questionJSON = JsonUtility.ToJson(questionDto);
+                        var dummyNetworkManager = (DummyServerNetworkManager)this.networkManager;
+                        dummyNetworkManager.OnSentDataToClient += (sender, args) =>
+                            {
+                                var command = NetworkCommandData.Parse(args.Message);
+
+                                if (
+                                    command.Name == "LoadQuestion" &&
+                                    command.Options["QuestionJSON"] == questionJSON &&
+                                    command.Options["TimeToAnswer"].ConvertTo<int>() < this.timeToAnswer)
+                                {
+                                    IntegrationTest.Pass();
+                                }
+                            };
+
+                        dummyServer.MainPlayersConnectionIds = new int[] { 1, 3, 4, 5 };
+                        dummyNetworkManager.FakeDisconnectPlayer(2);
+
+                        this.CoroutineUtils.WaitForFrames(1,
+                            () =>
+                                {
+                                    dummyServer.MainPlayersConnectionIds = new int[] { 1, 2, 3, 4, 5 };
+                                    dummyNetworkManager.FakeConnectPlayer(2);
+                                });
+                    });
+        }
+    }
+}
---
+++ Assets/Tests/Network/VoteForAnswerForCurrentQuestionColletor/WhenStartedAndAudiencePlayerReconnectedDontSendQuestion.cs
@@ -0,0 +1,62 @@
+﻿using DummyEveryBodyVsTheTeacherServer = Tests.DummyObjects.DummyEveryBodyVsTheTeacherServer;
+using DummyServerNetworkManager = Tests.DummyObjects.DummyServerNetworkManager;
+using ExtendedMonoBehaviour = Utils.Unity.ExtendedMonoBehaviour;
+
+namespace Assets.Tests.Network.VoteForAnswerForCurrentQuestionColletor
+{
+
+    using Commands;
+
+    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
+
+    using UnityEngine;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject.Source.Usage;
+
+    public class WhenStartedAndAudiencePlayerReconnectedDontSendQuestion : ExtendedMonoBehaviour
+    {
+        [Inject]
+        private IServerNetworkManager networkManager;
+
+        [Inject]
+        private IEveryBodyVsTheTeacherServer server;
+
+        [Inject]
+        private ICollectVoteResultForAnswerForCurrentQuestion voteResultCollector;
+
+        void Start()
+        {
+            var dummyServer = (DummyEveryBodyVsTheTeacherServer)this.server;
+            dummyServer.MainPlayersConnectionIds = new int[] {1, 2, 3};
+            dummyServer.PresenterId = 4;
+
+            this.voteResultCollector.StartCollecting();
+
+            this.CoroutineUtils.WaitForFrames(1,
+                () =>
+                    {
+                        var dummyNetworkManager = (DummyServerNetworkManager)this.networkManager;
+                        dummyNetworkManager.OnSentDataToClient += (sender, args) =>
+                            {
+                                var command = NetworkCommandData.Parse(args.Message);
+                                if (command.Name == "LoadQuestion")
+                                {
+                                    IntegrationTest.Fail();
+                                }
+                            };
+
+                        dummyNetworkManager.FakeDisconnectPlayer(10);
+
+                        this.CoroutineUtils.WaitForFrames(1,
+                            () =>
+                                {
+                                    dummyNetworkManager.FakeConnectPlayer(10);
+                                    this.CoroutineUtils.WaitForFrames(1, IntegrationTest.Pass);
+                                });
+                    });
+        }
+    }
+}
