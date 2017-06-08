c37b670d7ed5570ddb54234dabd867be5d0581d3
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-08 13:03:01 +0300

    RemoteStateActivator implemented

--- Assets/Scripts/Network/RemoteStateActivator.cs
+++ Assets/Scripts/Network/RemoteStateActivator.cs
@@ -2,9 +2,11 @@
 
 namespace Assets.Scripts.Network
 {
-
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
+    using Assets.Scripts.Commands;
     using Assets.Scripts.Interfaces;
     using Assets.Scripts.Interfaces.Network;
 
@@ -16,6 +18,8 @@ namespace Assets.Scripts.Network
 
         private readonly StateMachine stateMachine;
 
+        private readonly HashSet<string> bindedStatesCommandNames = new HashSet<string>();
+ 
         public RemoteStateActivator(IClientNetworkManager networkManager, StateMachine stateMachine)
         {
             if (networkManager == null)
@@ -34,23 +38,56 @@ namespace Assets.Scripts.Network
 
         public void Bind(string id, IState stateToActivate)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException("id");
+            }
+
+            if (stateToActivate == null)
+            {
+                throw new ArgumentNullException("stateToActivate");
+            }
+
+            if (this.networkManager.CommandsManager.Exists("Activate" + id))
+            {
+                throw new InvalidOperationException("Id already used");
+            }
+
+            var commandName = "Activate" + id;
+            var activateStateCommand = new ActivateStateCommand(this.stateMachine, stateToActivate);
+            this.networkManager.CommandsManager.AddCommand(commandName, activateStateCommand);
+            this.bindedStatesCommandNames.Add(commandName);
         }
 
         public void UnBind(string id)
         {
-            throw new System.NotImplementedException();
-        }
+            var commandName = "Activate" + id;
 
-        public void UnBind(IState state)
-        {
-            throw new System.NotImplementedException();
+            if (!this.networkManager.CommandsManager.Exists(commandName))
+            {
+                throw new ArgumentException("There arent any states associated with " + id);
+            }
+            
+            this.networkManager.CommandsManager.RemoveCommand(commandName);
+            this.bindedStatesCommandNames.Remove(commandName);
         }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
-        }
+            var bindedStatesCommandNames = this.bindedStatesCommandNames.ToArray();
 
+            for (int i = 0; i < bindedStatesCommandNames.Length; i++)
+            {
+                var commandName = bindedStatesCommandNames[i];
+                if (this.networkManager.CommandsManager.Exists(commandName))
+                {
+                    this.networkManager.CommandsManager.RemoveCommand(commandName);
+                }
+
+                this.bindedStatesCommandNames.Remove(commandName);
+            }
+
+            this.bindedStatesCommandNames.Clear();
+        }
     }
 }
