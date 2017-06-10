10b924775ca1db20bcaf464ff47c99e019a2aa62
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-10 20:18:56 +0300

    Pause and resume SecondsRemainingUIController commands

---
+++ Assets/Scripts/Commands/UI/PauseSecondsRemainingCommand.cs
@@ -0,0 +1,30 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Commands.UI
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces.Controllers;
+
+    public class PauseSecondsRemainingCommand : INetworkManagerCommand
+    {
+        private readonly ISecondsRemainingUIController secondsRemainingUIController;
+
+        public PauseSecondsRemainingCommand(ISecondsRemainingUIController secondsRemainingUIController)
+        {
+            if (secondsRemainingUIController == null)
+            {
+                throw new ArgumentNullException("secondsRemainingUIController");
+            }
+
+            this.secondsRemainingUIController = secondsRemainingUIController;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            this.secondsRemainingUIController.Paused = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Commands/UI/ResumeSecondsRemainingCommand.cs
@@ -0,0 +1,30 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Commands.UI
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Interfaces.Controllers;
+
+    public class ResumeSecondsRemainingCommand : INetworkManagerCommand
+    {
+        private readonly ISecondsRemainingUIController secondsRemainingUIController;
+
+        public ResumeSecondsRemainingCommand(ISecondsRemainingUIController secondsRemainingUIController)
+        {
+            if (secondsRemainingUIController == null)
+            {
+                throw new ArgumentNullException("secondsRemainingUIController");
+            }
+
+            this.secondsRemainingUIController = secondsRemainingUIController;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            this.secondsRemainingUIController.Paused = false;
+        }
+    }
+}
