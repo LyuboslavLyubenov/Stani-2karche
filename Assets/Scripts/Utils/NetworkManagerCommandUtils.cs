e2b7aa745b611e43aeced6689a4972f12e2b9ba0
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-11 14:26:04 +0300

    Utils for getting command name

---
+++ Assets/Scripts/Utils/NetworkManagerCommandUtils.cs
@@ -0,0 +1,16 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Utils
+{
+    public class NetworkManagerCommandUtils
+    {
+        NetworkManagerCommandUtils()
+        {    
+        }
+
+        public static string GetCommandName<TCommand>() where TCommand : INetworkManagerCommand
+        {
+            return typeof(TCommand).Name.Replace("Command", "");
+        }
+    }
+}
