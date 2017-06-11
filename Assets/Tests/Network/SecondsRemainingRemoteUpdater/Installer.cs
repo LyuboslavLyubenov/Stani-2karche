6185ce16033e48acfd3b532a52e402bfd9d91c33
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-11 14:31:02 +0300

    installer

---
+++ Assets/Tests/Network/SecondsRemainingRemoteUpdater/Installer.cs
@@ -0,0 +1,30 @@
+﻿using DummyEveryBodyVsTheTeacherServer = Tests.DummyObjects.DummyEveryBodyVsTheTeacherServer;
+using DummyServerNetworkManager = Tests.DummyObjects.DummyServerNetworkManager;
+
+namespace Assets.Tests.Network.SecondsRemainingRemoteUpdater
+{
+
+    using Assets.Scripts.Interfaces.Network;
+    using Assets.Scripts.Network;
+
+    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
+
+    using Zenject.Source.Install;
+
+    public class Installer : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            this.Container.Bind<IServerNetworkManager>()
+                .FromInstance(DummyServerNetworkManager.Instance);
+
+            this.Container.Bind<IEveryBodyVsTheTeacherServer>()
+                .To<DummyEveryBodyVsTheTeacherServer>()
+                .AsSingle();
+
+            this.Container.Bind<IRemoteSecondsRemainingUIUpdater>()
+                .To<RemoteSecondsRemainingUICommandsSender>();
+        }
+    }
+}
