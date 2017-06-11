8d259d052b31a09980dc63d0fed17d3b2303a59d
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-11 14:30:37 +0300

    Tests for SecondsRemainingUIControllerUpdater

---
+++ Assets/Tests/Network/SecondsRemainingRemoteUpdater/SendResumeCommandWhenNoMainPlayersConnectedAndThenMainPlayerConnected.cs
@@ -0,0 +1,60 @@
+﻿using DummyEveryBodyVsTheTeacherServer = Tests.DummyObjects.DummyEveryBodyVsTheTeacherServer;
+using DummyServerNetworkManager = Tests.DummyObjects.DummyServerNetworkManager;
+using ExtendedMonoBehaviour = Utils.Unity.ExtendedMonoBehaviour;
+
+namespace Assets.Tests.Network.SecondsRemainingRemoteUpdater
+{
+    using Assets.Scripts.Commands.UI;
+    using Assets.Scripts.Interfaces.Network;
+    using Assets.Scripts.Utils;
+    using Assets.Tests.Extensions;
+
+    using Commands;
+
+    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject.Source.Usage;
+
+    public class SendResumeCommandWhenNoMainPlayersConnectedAndThenMainPlayerConnected : ExtendedMonoBehaviour
+    {
+        [Inject]
+        private IServerNetworkManager networkManager;
+
+        [Inject]
+        private IEveryBodyVsTheTeacherServer server;
+
+        [Inject]
+        private IRemoteSecondsRemainingUIUpdater remoteSecondsRemainingUIUpdater;
+
+        void Start()
+        {
+            var dummyServer = (DummyEveryBodyVsTheTeacherServer)this.server;
+            var dummyNetworkManager = (DummyServerNetworkManager)this.networkManager;
+
+            dummyServer.MainPlayersConnectionIds = new int[] {};
+            dummyNetworkManager.FakeDisconnectPlayer(1);            
+
+            this.CoroutineUtils.WaitForFrames(1,
+                () =>
+                    {
+                        dummyNetworkManager.OnSentDataToClient += (sender, args) =>
+                            {
+                                var command = NetworkCommandData.Parse(args.Message);
+                                if (command.Name == NetworkManagerCommandUtils.GetCommandName<ResumeSecondsRemainingCommand>())
+                                {
+                                    IntegrationTest.Pass();
+                                }
+                            };
+
+                        dummyServer.MainPlayersConnectionIds = new int[]
+                                                               {
+                                                                   1
+                                                               };
+                        dummyNetworkManager.SimulateMainPlayerConnected(1, "Main player 1");
+                    });
+        }
+    }
+}
