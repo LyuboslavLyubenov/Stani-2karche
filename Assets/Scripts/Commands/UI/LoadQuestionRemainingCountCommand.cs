8163a78cbf6deecd15f6afe075e78c6111650155
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-15 14:51:27 +0300

    Command updating QuestionsRemainingUIController

---
+++ Assets/Scripts/Commands/UI/LoadQuestionRemainingCountCommand.cs
@@ -0,0 +1,33 @@
+﻿using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+
+namespace Assets.Scripts.Commands.UI
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Extensions;
+    using Assets.Scripts.Interfaces.Controllers;
+
+    public class LoadQuestionRemainingCountCommand : INetworkManagerCommand
+    {
+        private readonly IQuestionsRemainingUIController questionsRemainingUIController;
+
+        public LoadQuestionRemainingCountCommand(IQuestionsRemainingUIController questionsRemainingUIController)
+        {
+            if (questionsRemainingUIController == null)
+            {
+                throw new ArgumentNullException("questionsRemainingUIController");
+            }
+
+            this.questionsRemainingUIController = questionsRemainingUIController;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            var count = commandsOptionsValues["Count"]
+                .ConvertTo<int>();
+            this.questionsRemainingUIController.SetRemainingQuestions(count);
+        }
+    }
+}
