5dae2cbded18a172c05b3237b8d62515bee37a7c
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-19 22:21:32 +0300

    Surrender command added delay between sending GameEndCommand and Kick from server

--- Assets/Scripts/Commands/EveryBodyVsTheTeacher/SurrenderCommand.cs
+++ Assets/Scripts/Commands/EveryBodyVsTheTeacher/SurrenderCommand.cs
@@ -4,14 +4,18 @@ using IGameDataIterator = Interfaces.GameData.IGameDataIterator;
 using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
 using IServerNetworkManager = Interfaces.Network.NetworkManager.IServerNetworkManager;
 using NetworkCommandData = Commands.NetworkCommandData;
+using ThreadUtils = Utils.ThreadUtils;
 
 namespace Assets.Scripts.Commands.EveryBodyVsTheTeacher
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
     using Assets.Scripts.Extensions;
+
+    using UnityEngine;
 
     public class SurrenderCommand : INetworkManagerCommand
     {
@@ -44,20 +48,14 @@ namespace Assets.Scripts.Commands.EveryBodyVsTheTeacher
             this.iterator = iterator;
         }
 
-        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        private IEnumerator SurrenderCoroutine(int connectionId)
         {
-            var connectionId = commandsOptionsValues["ConnectionId"].ConvertTo<int>();
-
-            var isMainPlayer = this.server.ConnectedMainPlayersConnectionIds.Contains(connectionId);
-            if (!isMainPlayer || this.server.IsGameOver || !this.server.StartedGame)
-            {
-                return;
-            }
-
             var gameEndCommand = NetworkCommandData.From<GameEndCommand>();
             gameEndCommand.AddOption("Mark", this.iterator.CurrentMark.ToString());
             this.networkManager.SendClientCommand(connectionId, gameEndCommand);
 
+            yield return new WaitForSeconds(1f);
+            
             this.networkManager.KickPlayer(connectionId, "Успешно се отказа за игра.");
             this.networkManager.BanPlayer(connectionId);
 
@@ -69,7 +67,20 @@ namespace Assets.Scripts.Commands.EveryBodyVsTheTeacher
             if (areAllMainPlayersBanned)
             {
                 this.server.EndGame();
-            } 
+            }
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            var connectionId = commandsOptionsValues["ConnectionId"].ConvertTo<int>();
+
+            var isMainPlayer = this.server.ConnectedMainPlayersConnectionIds.Contains(connectionId);
+            if (!isMainPlayer || this.server.IsGameOver || !this.server.StartedGame)
+            {
+                return;
+            }
+
+            ThreadUtils.Instance.RunOnMainThread(this.SurrenderCoroutine(connectionId));
         }
     }
 }
