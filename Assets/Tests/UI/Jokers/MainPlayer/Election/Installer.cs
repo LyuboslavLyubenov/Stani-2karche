bc743d8cc08c544e3aeb2debb984407e6b68856f
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-06-25 15:40:09 +0300

    ElectionJokerUI for MainPlayers tests

---
+++ Assets/Tests/UI/Jokers/MainPlayer/Election/Installer.cs
@@ -0,0 +1,36 @@
+﻿using DummyClientNetworkManager = Tests.DummyObjects.DummyClientNetworkManager;
+
+namespace Assets.Tests.UI.Jokers.MainPlayer.Election
+{
+
+    using Interfaces.Network.NetworkManager;
+
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    using Zenject.Source.Install;
+
+    public class Installer : MonoInstaller
+    {
+        [SerializeField]
+        private Button thumbsUpButton;
+
+        [SerializeField]
+        private Button thumbsDownButton;
+
+        public override void InstallBindings()
+        {
+            this.Container.Bind<IClientNetworkManager>()
+                .To<DummyClientNetworkManager>()
+                .AsSingle();
+
+            this.Container.Bind<Button>()
+                .WithId("ThumbsUpButton")
+                .FromInstance(this.thumbsUpButton);
+
+            this.Container.Bind<Button>()
+                .WithId("ThumbsDownButton")
+                .FromInstance(this.thumbsDownButton);
+        }
+    }
+}
---
+++ Assets/Tests/UI/Jokers/MainPlayer/Election/WhenClickedThumbsUpSendVotedForJokerCommand.cs
@@ -0,0 +1,56 @@
+﻿using DummyClientNetworkManager = Tests.DummyObjects.DummyClientNetworkManager;
+using ExtendedMonoBehaviour = Utils.Unity.ExtendedMonoBehaviour;
+
+namespace Assets.Tests.UI.Jokers.MainPlayer.Election
+{
+
+    using Assets.Scripts.Controllers.EveryBodyVsTheTeacher.Jokers.Election.MainPlayer;
+    using Assets.Scripts.Extensions.Unity.UI;
+
+    using Commands;
+
+    using Interfaces.Network.NetworkManager;
+
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject.Source.Usage;
+
+    public class WhenClickedThumbsUpSendVotedForJokerCommand : ExtendedMonoBehaviour
+    {
+        [Inject]
+        private IClientNetworkManager networkManager;
+
+        [Inject(Id = "ThumbsUpButton")]
+        private Button thumbsUpButton;
+        
+        void Start()
+        {
+            var jokerElectionUI = GameObject.FindObjectOfType<JokerElectionUIController>().gameObject;
+            jokerElectionUI.SetActive(false);
+
+            this.CoroutineUtils.WaitForFrames(1,
+                () =>
+                    {
+                        jokerElectionUI.SetActive(true);
+
+                        this.CoroutineUtils.WaitForFrames(1,
+                            () =>
+                                {
+                                    var dummyNetworkManager = (DummyClientNetworkManager)this.networkManager;
+                                    dummyNetworkManager.OnSentToServerMessage += (sender, args) =>
+                                        {
+                                            var command = NetworkCommandData.Parse(args.Message);
+                                            if (command.Name == "PlayerVotedFor")
+                                            {
+                                                IntegrationTest.Pass();
+                                            }
+                                        };
+                                    this.thumbsUpButton.SimulateClick();
+                                });
+                    });
+        }
+    }
+}
