caba96c80e1c206a47e9f0e284c04d73cd8a68d3
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-07-05 12:30:36 +0300

    Not getting correct joker name MainPlayerJoker fixed

--- Assets/Scripts/Extensions/IJokerExtensions.cs
+++ Assets/Scripts/Extensions/IJokerExtensions.cs
@@ -2,11 +2,22 @@
 {
 
     using System;
+    using System.Linq;
 
     using Assets.Scripts.Interfaces;
+    using Assets.Scripts.Jokers.EveryBodyVsTheTeacher.MainPlayer;
 
     public static class IJokerExtensions
     {
+        private static string GetMainPlayerJokerName(Type jokerType)
+        {
+            var jokerName = jokerType.GetGenericArguments()
+                .First()
+                .Name
+                .Replace("Joker", "");
+            return jokerName;
+        }
+
         public static string GetName(this IJoker joker)
         {
             if (joker == null)
@@ -14,8 +25,21 @@
                 throw new ArgumentNullException();
             }
 
-            return joker.GetType()
-                .Name.Replace("Joker", "");
+            var jokerType = joker.GetType();
+            
+            if (jokerType.IsGenericType)
+            {
+                var jokerName = jokerType.GetGenericTypeDefinition()
+                    .Name;
+                var friendlyJokerName = jokerName.Remove(jokerName.IndexOf("`"));
+
+                if (friendlyJokerName == "MainPlayerJoker")
+                {
+                    return GetMainPlayerJokerName(jokerType);
+                }
+            }
+
+            return jokerType.Name.Replace("Joker", "");
         }
     }
 }
