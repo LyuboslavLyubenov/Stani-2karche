aebd63e0f6e9b96fb192c382750a9963b023b1dc
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-07-13 13:40:52 +0300

    Implemented CreatedGameInfoSender using NetworkManager

---
+++ Assets/Scripts/Commands/CreatedGameInfoSender/SendGameInfoCommand.cs
@@ -0,0 +1,51 @@
+﻿using IGameServer = Interfaces.IGameServer;
+using INetworkManagerCommand = Interfaces.Network.NetworkManager.INetworkManagerCommand;
+using IServerNetworkManager = Interfaces.Network.NetworkManager.IServerNetworkManager;
+using NetworkCommandData = Commands.NetworkCommandData;
+
+namespace Assets.Scripts.Commands.CreatedGameInfoSender
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    using Assets.Scripts.Commands.CreatedGameInfoReceiver;
+    using Assets.Scripts.Extensions;
+
+    using UnityEngine;
+
+    public class SendGameInfoCommand : INetworkManagerCommand
+    {
+        private readonly IServerNetworkManager networkManager;
+
+        private readonly IGameServer gameServer;
+
+        public SendGameInfoCommand(IServerNetworkManager networkManager, IGameServer gameServer)
+        {
+            if (networkManager == null)
+            {
+                throw new ArgumentNullException("networkManager");
+            }
+
+            if (gameServer == null)
+            {
+                throw new ArgumentNullException("gameServer");
+            }
+
+            this.networkManager = networkManager;
+            this.gameServer = gameServer;
+        }
+
+        public void Execute(Dictionary<string, string> commandsOptionsValues)
+        {
+            var connectionId = commandsOptionsValues["ConnectionId"].ConvertTo<int>();
+            var gameInfo = GameInfoFactory.Instance.Get(this.networkManager, this.gameServer);
+            var gameInfoJSON = JsonUtility.ToJson(gameInfo);
+            var receivedGameInfoCommand = NetworkCommandData.From<ReceivedGameInfoCommand>();
+            receivedGameInfoCommand.AddOption("GameInfoJSON", gameInfoJSON);
+            this.networkManager.SendClientCommand(connectionId, receivedGameInfoCommand);
+
+            this.networkManager.KickPlayer(connectionId);
+        }
+    }
+}
---
+++ Assets/Scripts/Network/GameInfo/New/CreatedGameInfoSender.cs
@@ -0,0 +1,40 @@
+﻿using ClientConnectionIdEventArgs = EventArgs.ClientConnectionIdEventArgs;
+using ICreatedGameInfoSender = Interfaces.Network.NetworkManager.ICreatedGameInfoSender;
+using IGameServer = Interfaces.IGameServer;
+using IServerNetworkManager = Interfaces.Network.NetworkManager.IServerNetworkManager;
+
+namespace Assets.Scripts.Network.GameInfo.New
+{
+    using System;
+
+    using Assets.Scripts.Commands.CreatedGameInfoSender;
+
+    public class CreatedGameInfoSender : ICreatedGameInfoSender
+    {
+        private readonly IServerNetworkManager networkManager;
+
+        private readonly SendGameInfoCommand sendGameInfoCommand;
+
+        public CreatedGameInfoSender(IServerNetworkManager networkManager, IGameServer gameServer)
+        {
+            if (networkManager == null)
+            {
+                throw new ArgumentNullException("networkManager");
+            }
+
+            if (gameServer == null)
+            {
+                throw new ArgumentNullException("gameServer");
+            }
+
+            this.networkManager = networkManager;
+            this.sendGameInfoCommand = new SendGameInfoCommand(networkManager, gameServer);
+            this.networkManager.CommandsManager.AddCommand(this.sendGameInfoCommand);
+        }
+
+        public void Dispose()
+        {
+            this.networkManager.CommandsManager.RemoveCommand(this.sendGameInfoCommand);
+        }
+    }
+}
