613bb2b016766a043acde50dc5fbfcc6c65bb0c2
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-07-13 19:27:46 +0300

    LobbyUIController remove unused references

--- Assets/Scripts/Controllers/Lobby/LobbyUIController.cs
+++ Assets/Scripts/Controllers/Lobby/LobbyUIController.cs
@@ -1,18 +1,15 @@
 ﻿// ReSharper disable ArrangeTypeMemberModifiers
 namespace Controllers.Lobby
 {
-
-    using Interfaces.Network;
+    using Interfaces.Network.NetworkManager;
     using Interfaces.Services;
-
-    using Network.GameInfo;
-
+    
     using UnityEngine;
     using UnityEngine.SceneManagement;
 
     using Utils;
 
-    using Zenject.Source.Usage;
+    using Zenject;
 
     public class LobbyUIController : MonoBehaviour
     {
@@ -26,13 +23,7 @@ namespace Controllers.Lobby
         private ILANServersDiscoverer IlanServersDiscoverer;
         
         [Inject]
-        private CreatedGameInfoReceiver createdGameInfoReceiver;
-
-        [Inject]
-        private ISimpleTcpClient tcpClient;
-
-        [Inject]
-        private ISimpleTcpServer tcpServer;
+        private ICreatedGameInfoReceiver createdGameInfoReceiver;
         
         void Awake()
         {
@@ -58,11 +49,7 @@ namespace Controllers.Lobby
         private void Dispose()
         {
             this.IlanServersDiscoverer.Dispose();
-            this.tcpServer.Dispose();
-            this.tcpClient.Dispose();
-
-            this.tcpServer = null;
-            this.tcpClient = null;
+            
             this.IlanServersDiscoverer = null;
 
             this.createdGameInfoReceiver = null;
