e6114fa692c341a220f203c5be0f12e0a8cece19
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-08-21 14:36:42 +0300

    Tests for "display image as answer" feature

---
+++ Assets/Tests/UI/QuestionUI/Installer.cs
@@ -0,0 +1,23 @@
+﻿namespace Assets.Tests.UI.QuestionUI
+{
+
+    using Controllers;
+
+    using Interfaces.Controllers;
+
+    using UnityEngine;
+
+    using Zenject;
+
+    public class Installer : MonoInstaller
+    {
+        [SerializeField]
+        private QuestionUIController questionUIController;
+
+        public override void InstallBindings()
+        {
+            this.Container.Bind<IQuestionUIController>()
+                .FromInstance(this.questionUIController);
+        }
+    }
+}
---
+++ Assets/Tests/UI/QuestionUI/WhenAnswerIsPathAndIsValidImageShowImageAsAnswer.cs
@@ -0,0 +1,55 @@
+﻿using ExtendedMonoBehaviour = Utils.Unity.ExtendedMonoBehaviour;
+
+namespace Assets.Tests.UI.QuestionUI
+{
+
+    using System.Collections;
+    using System.Reflection;
+
+    using Assets.Tests.Utils;
+    
+    using Interfaces;
+    using Interfaces.Controllers;
+
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    using UnityTestTools.IntegrationTestsFramework.TestRunner;
+
+    using Zenject;
+
+    public class WhenAnswerIsPathAndIsValidImageShowImageAsAnswer : MonoBehaviour
+    {
+        [Inject]
+        private IQuestionUIController questionUIController;
+
+        void Start()
+        {
+            this.StartCoroutine(this.Test());
+        }
+
+        private IEnumerator Test()
+        {
+            yield return new WaitForSeconds(1);
+
+            var question = new QuestionGenerator().GenerateQuestion();
+            question.Answers[0] = "images\\testimage.jpg";
+            this.questionUIController.LoadQuestion(question);
+
+            yield return new WaitForSeconds(3);
+
+            var answerObj = GameObject.FindObjectOfType<Button>();
+            var imageComponent = answerObj.transform.GetChild(0)
+                .GetComponent<Image>();
+
+            if (imageComponent != null)
+            {
+                IntegrationTest.Pass();
+            }
+            else
+            {
+                IntegrationTest.Fail();
+            }
+        }
+    }
+}
