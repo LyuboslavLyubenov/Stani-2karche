0ccbdbb1eccb633055ebda253f6d5819593fa3c6
Lyuboslav Lyubenov <lubidead1@mail.bg>
2017-08-30 23:01:24 +0300

    Unity scene manager wrapper

---
+++ Assets/Scripts/Utils/SceneManager.cs
@@ -0,0 +1,37 @@
+﻿namespace Assets.Scripts.Utils
+{
+
+    using System;
+
+    using UnityEngine.Internal;
+    using UnityEngine.SceneManagement;
+
+    public class SceneManager : UnityEngine.SceneManagement.SceneManager
+    {
+        public static event EventHandler OnBeforeLoadScene = delegate { };
+
+        public new static void LoadScene(string sceneName)
+        {
+            OnBeforeLoadScene(null, EventArgs.Empty);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+
+        public new static void LoadScene(string sceneName, [DefaultValue("LoadSceneMode.Single")] LoadSceneMode mode)
+        {
+            OnBeforeLoadScene(null, EventArgs.Empty);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, mode);
+        }
+
+        public new static void LoadScene(int sceneBuildIndex)
+        {
+            OnBeforeLoadScene(null, EventArgs.Empty);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex);
+        }
+
+        public new static void LoadScene(int sceneBuildIndex, [DefaultValue("LoadSceneMode.Single")] LoadSceneMode mode)
+        {
+            OnBeforeLoadScene(null, EventArgs.Empty);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex, mode);
+        }
+    }
+}
