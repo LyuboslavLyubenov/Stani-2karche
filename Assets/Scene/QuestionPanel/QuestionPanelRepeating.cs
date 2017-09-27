3103e53ba8a1a42b56deaff2a22a1a65acf037fe
Lyuboslav Tihomirov Lyubenov <lubidead1@mail.bg>
2017-09-27 21:08:36 +0300

    QuestionPanel with repeating random questions - using for testing

---
+++ Assets/Scene/QuestionPanel/QuestionPanelRepeating.cs
@@ -0,0 +1,24 @@
+﻿using Controllers;
+using Assets.Tests.Utils;
+
+public class QuestionPanelRepeating : QuestionUIController
+{
+    private QuestionGenerator questionGenerator = new QuestionGenerator();
+
+	// Use this for initialization
+	void Awake () 
+    {
+        var question = this.questionGenerator.GenerateQuestion();
+        base.LoadQuestion(question);
+
+        base.OnAnswerClick += (sender, args) =>
+        {
+                base.CoroutineUtils.WaitForSeconds(1, this.LoadNewQuestion);
+        };
+	}
+
+    private void LoadNewQuestion()
+    {
+        base.LoadQuestion(questionGenerator.GenerateQuestion());
+    }	
+}
